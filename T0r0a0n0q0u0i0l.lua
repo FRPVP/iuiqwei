b3f18530cb0b498b2e959f64ff40117332fdc7d6
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-21 18:08:17 -0400

    Update T0r0a0n0q0u0i0l.lua

--- T0r0a0n0q0u0i0l.lua
+++ T0r0a0n0q0u0i0l.lua
@@ -6749,11 +6749,6 @@ end)
 end)
 
 
-Stipid = 13850207336
-Sbaseid = 12976059241
-local isHeOk = false -- Set the initial state to false
-local isSprayPaintInBackpack = false -- Keep track of whether spray paint is in backpack or not
-
 PremiumPS:CreateToggle("test", {Toggled=false , Description = false}, function(val)
     isHeOk = val -- Update the script state based on the toggle value
 
@@ -6761,15 +6756,10 @@ PremiumPS:CreateToggle("test", {Toggled=false , Description = false}, function(v
         if game.Players.LocalPlayer.Character ~= nil then
             for i, v in pairs(game.Players:GetPlayers()) do
                 if v ~= game.Players.LocalPlayer and v.Character ~= nil and v.Character:FindFirstChild("HumanoidRootPart") then
-                    if not isSprayPaintInBackpack then
-                        if game:GetService("Players").LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
-                            game:GetService("Players").LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
-                            isSprayPaintInBackpack = true
-                        end
-                    end
-
-                    if isSprayPaintInBackpack then
+                    if game:GetService("Players").LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
                         local posofpl = workspace[v.Name].HumanoidRootPart.CFrame
+                        -- Move the spray paint from backpack to character
+                        game:GetService("Players").LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Extras"):WaitForChild("ReplicateToy"):InvokeServer("SprayPaint")
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Extras"):WaitForChild("ReplicateToy"):InvokeServer("SprayPaint")
 game:GetService("Players").LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character	
@@ -6803,12 +6793,12 @@ game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(Sb
 game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(Sbaseid, Enum.NormalId.Bottom, 0.5, workspace[v.Name].HumanoidRootPart, posofpl * CFrame.new(-0.5,-1.3,-0.7))
 game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(Sbaseid, Enum.NormalId.Left, 0.5, workspace[v.Name].HumanoidRootPart, posofpl * CFrame.new(-0.65,-1.15,-0.7))
 game.Players.LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
-                        wait(15.5)
-                    end
                 end
             end
         end
     end
+    wait(15.5)
+end
 end)
 
 			
