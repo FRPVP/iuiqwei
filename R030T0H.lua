757ff6f24a71e319b2d3394aaa282702ea06f4e0
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-21 14:17:15 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,9 +1,8 @@
-for _, player in ipairs(game.Players:GetPlayers()) do
-    if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        local posOfPenis = player.Character.HumanoidRootPart.CFrame
-
-        -- Spray painting the penis on all sides
-        for _, normalId in ipairs(Enum.NormalId:GetEnumItems()) do
+for _, normalId in ipairs(Enum.NormalId:GetEnumItems()) do
+    for _, player in ipairs(game.Players:GetPlayers()) do
+        if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+            local posOfPenis = player.Character.HumanoidRootPart.CFrame
+            
             game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Extras"):WaitForChild("ReplicateToy"):InvokeServer("SprayPaint")
             game.Players.LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
 
@@ -49,6 +48,7 @@ for _, player in ipairs(game.Players:GetPlayers()) do
             game.Players.LocalPlayer.Character.SprayPaint.Remote:FireServer(12976059241, Enum.NormalId.Front, 0.5, player.Character.HumanoidRootPart, posOfPenis * CFrame.new(-0.5,-1.15,-0.85)) 
             game.Players.LocalPlayer.Character.SprayPaint.Remote:FireServer(12976059241, Enum.NormalId.Bottom, 0.5, player.Character.HumanoidRootPart, posOfPenis * CFrame.new(-0.5,-1.3,-0.7)) 
             game.Players.LocalPlayer.Character.SprayPaint.Remote:FireServer(12976059241, Enum.NormalId.Left, 0.5, player.Character.HumanoidRootPart, posOfPenis * CFrame.new(-0.65,-1.15,-0.7)) 
+        wait(0.1) -- Add a small delay to prevent flooding the server with requests
         end
     end
 end
