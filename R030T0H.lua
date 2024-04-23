4854568deba10a59469e5d269dd3d7bd211677cc
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-23 19:27:13 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,34 +1,34 @@
-function poolplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (poolplayertarget.Character.Head), poolplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+function resetplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (resetplayertarget.Character.Head), resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
 end
 
 local Toggle = Tabs.Premium:AddToggle("", {Title = "Blind", Default = false })
 
-Toggle:OnChanged(function(poolplayer)
-    if poolplayer == true then
-        poolplayerloop = true
-        while poolplayerloop do
-            function poolplayerloopfix()
+Toggle:OnChanged(function(resetplayer)
+    if resetplayer == true then
+        resetplayerloop = true
+        while resetplayerloop do
+            function resetplayerloopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if fetargetname == "All" then
                     for _, v in pairs(players:GetPlayers()) do
-                        poolplayertarget = players:FindFirstChild(v.Name)
-                        poolplayerfunc()
+                        resetplayertarget = players:FindFirstChild(v.Name)
+                        resetplayerfunc()
                         task.wait()
                     end
                 else
-                    poolplayertarget = players:FindFirstChild(fetargetname)
-                    poolplayerfunc()
+                    resetplayertarget = players:FindFirstChild(fetargetname)
+                    resetplayerfunc()
                 end
                 task.wait(0)
             end
             wait()
-            pcall(poolplayerloopfix)
+            pcall(resetplayerloopfix)
         end
     end
-    if poolplayer == false then
-        poolplayerloop = false
+    if resetplayer == false then
+        resetplayerloop = false
         wait()
     end
 end)
