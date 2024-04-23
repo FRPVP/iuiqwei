4951f82ff368d252998c7160fa154c9893be5e2e
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-23 19:28:52 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2593,7 +2593,42 @@ Options.MyToggle:SetValue(false)
 
 
 
+function resetplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (resetplayertarget.Character.Head), resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+end
 
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Loop Reset", Default = false })
+
+Toggle:OnChanged(function(resetplayer)
+    if resetplayer == true then
+        resetplayerloop = true
+        while resetplayerloop do
+            function resetplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        resetplayertarget = players:FindFirstChild(v.Name)
+                        resetplayerfunc()
+                        task.wait()
+                    end
+                else
+                    resetplayertarget = players:FindFirstChild(fetargetname)
+                    resetplayerfunc()
+                end
+                task.wait(0)
+            end
+            wait()
+            pcall(resetplayerloopfix)
+        end
+    end
+    if resetplayer == false then
+        resetplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
 
 
 	
