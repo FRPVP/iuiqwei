5b6e2ac2d3c8ba96af82bca3daa3c30d30b8d3bd
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 19:32:44 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2607,8 +2607,54 @@ Options.MyToggle:SetValue(false)
 
 
 
+
+
+
+function reerplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Bottom, 6.331, (reerplayertarget.Character.HumanoidRootPart), reerplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0)
+end
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Null", Default = false })
+
+Toggle:OnChanged(function(reerplayer)
+    if reerplayer == true then
+        reerplayerloop = true
+        while reerplayerloop do
+            function reerplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            reerplayertarget = players:FindFirstChild(v.Name)
+                            reerplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    reerplayertarget = players:FindFirstChild(fetargetname)
+                    reerplayerfunc()
+                end
+                task.wait(0)
+            end
+            wait()
+            pcall(reerplayerloopfix)
+        end
+    end
+    if reerplayer == false then
+        reerplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
 	
-	
+
+
+
+
+
+
 end
 
 
