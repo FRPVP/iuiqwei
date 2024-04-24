2e4204d2a806b7b17a6864f5a8cde5829e530363
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 19:56:27 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2610,38 +2610,44 @@ Options.MyToggle:SetValue(false)
 
 
 
-function reerplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Bottom, 6.331, (reerplayertarget.Character.HumanoidRootPart), reerplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
+function collideplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (collideplayertarget.Character.HumanoidRootPart), collideplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
 end
 
-Tabs.Premium:AddButton({
-    Title = "Test",
-    Description = "",
-    Icon = "", -- Replace "icon.png" with the path to your icon file
-    Callback = function()
-        -- Check if a name is selected
-        if fetargetname == "All" then
-            EquipSpray() -- Equip the spray first
-            -- Iterate over all players and execute heatplayerfunc on each player except yourself
-            for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-                if player ~= game:GetService("Players").LocalPlayer then
-                    reerplayerfunc(player)
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Remove Collide Sensors", Default = false })
+
+Toggle:OnChanged(function(collideplayer)
+    if collideplayer == true then
+        collideplayerloop = true
+        while collideplayerloop do
+            function collideplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            collideplayertarget = players:FindFirstChild(v.Name)
+                            collideplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    collideplayertarget = players:FindFirstChild(fetargetname)
+                    collideplayerfunc()
                 end
+                task.wait(15)
             end
-        elseif fetargetname ~= "" then
-            EquipSpray() -- Equip the spray first
-            -- Find the player with the selected name
-            local reerplayertarget = game:GetService("Players"):FindFirstChild(fetargetname)
-            if reerplayertarget then
-                reerplayerfunc(reerplayertarget) -- Execute heatplayerfunc on the player
-            else
-                print("Player not found.")
-            end
-        else
-            print("Please select a name from the dropdown.")
+            wait()
+            pcall(collideplayerloopfix)
         end
     end
-})
+    if collideplayer == false then
+        collideplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
 	
 
 
