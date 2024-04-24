31e667607bd67db053885989d636d39968a02734
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 19:23:11 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -2,7 +2,7 @@ function reerplayerfunc()
 game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Bottom, 6.331, (reerplayertarget.Character.HumanoidRootPart), reerplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0)
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Remove reerrs", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Remove Collides", Default = false })
 
 Toggle:OnChanged(function(reerplayer)
     if reerplayer == true then
