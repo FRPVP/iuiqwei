f5d04067e4f6b463c06f943d002951a4cc3df3f5
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-23 21:55:02 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2527,8 +2527,33 @@ Options.MyToggle:SetValue(false)
 
 
 
+	
+function heatplayerfunc(heatplayertarget)
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+end
 
+Tabs.Premium:AddButton({
+    Title = "heat",
+    Description = "",
+    Icon = "", -- Replace "icon.png" with the path to your icon file
+    Callback = function()
+        -- Check if a name is selected
+        if fetargetname ~= "" then
+            EquipSpray() -- Equip the spray first
+            -- Find the player with the selected name
+            local heatplayertarget = game:GetService("Players"):FindFirstChild(fetargetname)
+            if heatplayertarget then
+                heatplayerfunc(heatplayertarget) -- Execute heatplayerfunc on the player
+            else
+                print("Player not found.")
+            end
+        else
+            print("Please select a name from the dropdown.")
+        end
+    end
+})
 
+	
 
 
 
