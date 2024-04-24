ebcdf7437f0846c8d2ad3879a19b604e988e41a5
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 16:55:59 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2605,52 +2605,6 @@ end)
 Options.MyToggle:SetValue(false)
 
 
-
-
-
-
-function collideplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Bottom, 6.331, (collideplayertarget.Character.HumanoidRootPart), collideplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0)
-end
-
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Remove Colliders", Default = false })
-
-Toggle:OnChanged(function(collideplayer)
-    if collideplayer == true then
-        collideplayerloop = true
-        while collideplayerloop do
-            function collideplayerloopfix()
-                EquipSpray()
-                task.wait(0.4)
-                if fetargetname == "All" then
-                    for _, v in pairs(players:GetPlayers()) do
-                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
-                            collideplayertarget = players:FindFirstChild(v.Name)
-                            collideplayerfunc()
-                            task.wait()
-                        end
-                    end
-                else
-                    collideplayertarget = players:FindFirstChild(fetargetname)
-                    collideplayerfunc()
-                end
-                task.wait(15)
-            end
-            wait()
-            pcall(collideplayerloopfix)
-        end
-    end
-    if collideplayer == false then
-        collideplayerloop = false
-        wait()
-    end
-end)
-
-Options.MyToggle:SetValue(false)
-
-
-
-
 	
 	
 	
