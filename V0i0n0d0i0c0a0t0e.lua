66b03cb795e9a61097d486899d4beeb60c3fb30c
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 20:04:30 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2610,11 +2610,49 @@ Options.MyToggle:SetValue(false)
 
 
 
+function reerplayerfunc(reerplayertarget)
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (reerplayertarget.Character.HumanoidRootPart), reerplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
+end
+
+Tabs.Premium:AddButton({
+    Title = "Remove Collides (15sec)",
+    Description = "",
+    Icon = "", -- Replace "icon.png" with the path to your icon file
+    Callback = function()
+        -- Check if a name is selected
+        if fetargetname == "All" then
+            EquipSpray() -- Equip the spray first
+            -- Iterate over all players and execute reerplayerfunc on each player except yourself
+            for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+                if player ~= game:GetService("Players").LocalPlayer then
+                    reerplayerfunc(player)
+                end
+            end
+        elseif fetargetname ~= "" then
+            EquipSpray() -- Equip the spray first
+            -- Find the player with the selected name
+            local reerplayertarget = game:GetService("Players"):FindFirstChild(fetargetname)
+            if reerplayertarget then
+                reerplayerfunc(reerplayertarget) -- Execute reerplayerfunc on the player
+            else
+                print("Player not found.")
+            end
+        else
+            print("Please select a name from the dropdown.")
+        end
+    end
+})
+
+
+
+
+
+
 function collideplayerfunc()
 game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (collideplayertarget.Character.HumanoidRootPart), collideplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Remove Collide Sensors", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Loop Remove Collides", Default = false })
 
 Toggle:OnChanged(function(collideplayer)
     if collideplayer == true then
