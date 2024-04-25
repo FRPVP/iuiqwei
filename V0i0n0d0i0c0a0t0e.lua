4fd566d38699e0838eb3884c4354d8eeed05c483
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 21:54:14 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2738,7 +2738,7 @@ Options.MyToggle:SetValue(false)
 
 
 function trollplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10193511157, Enum.NormalId.Front, 1.5, (trollplayertarget.Character.HumanoidRootPart), trollplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,1,-1))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10193511157, Enum.NormalId.Front, 1.5, (trollplayertarget.Character.HumanoidRootPart), trollplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,1.2,-1))
 end
 
 local Toggle = Tabs.Premium:AddToggle("", {Title = "Troll Face", Default = false })
