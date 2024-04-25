40561aebb98480a8dc63f19d4269eac53ab9c756
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 20:19:23 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2694,7 +2694,7 @@ Options.MyToggle:SetValue(false)
 
 
 function faceplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(12579922286, Enum.NormalId.Bottom, 2048, (faceplayertarget.Character.HumanoidRootPart), faceplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 50, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(12579922286, Enum.NormalId.Bottom, 200, (faceplayertarget.Character.HumanoidRootPart), faceplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 500, 0))
 end
 
 local Toggle = Tabs.Premium:AddToggle("", {Title = "Creep Above Player", Default = false })
