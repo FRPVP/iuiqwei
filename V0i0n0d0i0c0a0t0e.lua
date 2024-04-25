46d26935765ab5dd828932014602ecd9b95f4351
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 21:34:48 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2737,6 +2737,48 @@ Options.MyToggle:SetValue(false)
 
 
 
+function trollplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10193511157, Enum.NormalId.Front, 8, (trollplayertarget.Character.HumanoidRootPart), trollplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,0))
+end
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Troll Face", Default = false })
+
+Toggle:OnChanged(function(trollplayer)
+    if trollplayer == true then
+        trollplayerloop = true
+        while trollplayerloop do
+            function trollplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            trollplayertarget = players:FindFirstChild(v.Name)
+                            trollplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    trollplayertarget = players:FindFirstChild(fetargetname)
+                    trollplayerfunc()
+                end
+                task.wait(15)
+            end
+            wait()
+            pcall(trollplayerloopfix)
+        end
+    end
+    if trollplayer == false then
+        trollplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
+
+	
+
+
 
 end
 
