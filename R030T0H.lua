e1d7766c0cb303e84f405ce11cee73bfff19047f
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 20:17:05 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,32 +1,38 @@
-function reerplayerfunc(reerplayertarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (reerplayertarget.Character.HumanoidRootPart), reerplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
+function faceplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(12579922286, Enum.NormalId.Bottom, 2048, (faceplayertarget.Character.HumanoidRootPart), faceplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 50, 0))
 end
 
-Tabs.Premium:AddButton({
-    Title = "Remove Collides (15sec)",
-    Description = "",
-    Icon = "", -- Replace "icon.png" with the path to your icon file
-    Callback = function()
-        -- Check if a name is selected
-        if fetargetname == "All" then
-            EquipSpray() -- Equip the spray first
-            -- Iterate over all players and execute reerplayerfunc on each player except yourself
-            for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-                if player ~= game:GetService("Players").LocalPlayer then
-                    reerplayerfunc(player)
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Creep Above Player", Default = false })
+
+Toggle:OnChanged(function(faceplayer)
+    if faceplayer == true then
+        faceplayerloop = true
+        while faceplayerloop do
+            function faceplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            faceplayertarget = players:FindFirstChild(v.Name)
+                            faceplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    faceplayertarget = players:FindFirstChild(fetargetname)
+                    faceplayerfunc()
                 end
+                task.wait(15)
             end
-        elseif fetargetname ~= "" then
-            EquipSpray() -- Equip the spray first
-            -- Find the player with the selected name
-            local reerplayertarget = game:GetService("Players"):FindFirstChild(fetargetname)
-            if reerplayertarget then
-                reerplayerfunc(reerplayertarget) -- Execute reerplayerfunc on the player
-            else
-                print("Player not found.")
-            end
-        else
-            print("Please select a name from the dropdown.")
+            wait()
+            pcall(faceplayerloopfix)
         end
     end
-})
+    if faceplayer == false then
+        faceplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
