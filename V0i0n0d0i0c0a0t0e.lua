67d612502d655ba9bcc45537ec6953b9f91cff1f
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-24 22:01:08 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2737,6 +2737,46 @@ Options.MyToggle:SetValue(false)
 
 
 
+
+function funnyplayerfunc(funnyplayertarget)
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10193511157, Enum.NormalId.Front, 1.5, (trollplayertarget.Character.HumanoidRootPart), trollplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,1.2,-1))
+end
+
+Tabs.Premium:AddButton({
+    Title = "Troll Face (15sec)",
+    Description = "",
+    Icon = "", -- Replace "icon.png" with the path to your icon file
+    Callback = function()
+        -- Check if a name is selected
+        if fetargetname == "All" then
+            EquipSpray() -- Equip the spray first
+            -- Iterate over all players and execute funnyplayerfunc on each player except yourself
+            for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+                if player ~= game:GetService("Players").LocalPlayer then
+                    funnyplayerfunc(player)
+                end
+            end
+        elseif fetargetname ~= "" then
+            EquipSpray() -- Equip the spray first
+            -- Find the player with the selected name
+            local funnyplayertarget = game:GetService("Players"):FindFirstChild(fetargetname)
+            if funnyplayertarget then
+                funnyplayerfunc(funnyplayertarget) -- Execute funnyplayerfunc on the player
+            else
+                print("Player not found.")
+            end
+        else
+            print("Please select a name from the dropdown.")
+        end
+    end
+})
+
+
+
+
+
+
+
 function trollplayerfunc()
 game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10193511157, Enum.NormalId.Front, 1.5, (trollplayertarget.Character.HumanoidRootPart), trollplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,1.2,-1))
 end
