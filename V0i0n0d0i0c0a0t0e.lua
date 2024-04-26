0793adf5fdb7348d965820c22918ba7074d0cb65
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-26 13:29:02 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -2942,6 +2942,52 @@ Options.MyToggle:SetValue(false)
 
 
 
+
+function communistplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(6978648716, Enum.NormalId.Front, 5, (communistplayertarget.Character.HumanoidRootPart), communistplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,5,0))
+end
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Communist", Default = false })
+
+Toggle:OnChanged(function(communistplayer)
+    if communistplayer == true then
+        communistplayerloop = true
+        while communistplayerloop do
+            function communistplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            communistplayertarget = players:FindFirstChild(v.Name)
+                            communistplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    communistplayertarget = players:FindFirstChild(fetargetname)
+                    communistplayerfunc()
+                end
+                task.wait(15)
+            end
+            wait()
+            pcall(communistplayerloopfix)
+        end
+    end
+    if communistplayer == false then
+        communistplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
+
+
+
+
+
+
+
 end
 
 
