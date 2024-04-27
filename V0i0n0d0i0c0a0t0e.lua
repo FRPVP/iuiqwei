9cb3a6f4761d854d6269cdf7dd1d288d6e9f77f9
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-27 16:52:58 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -5516,78 +5516,51 @@ end
 
 
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Broken Arm", Default = false})
-
-local knifeFunctionalityEnabled = false -- Track whether knife functionality is enabled
-
-local function ToggleKnifeFunction(enabled)
+Tabs.Animation:AddButton({
+    Title = "Break Your Arm",
+    Description = "you will soon realize it's literal (reset a couple of times to fix)",
+    Callback = function()
     local lp = game.Players.LocalPlayer
-    local knife = lp.Character:FindFirstChild("KnifeDisplay")
+local knife = lp.Character:WaitForChild("KnifeDisplay")
+knife.Massless = true
 
-    if enabled then
-        if not knife then
-            print("KnifeDisplay not found!")
-            return
-        end
-        knife.Massless = true
+local animation1 = Instance.new("Animation")
+animation1.AnimationId = "rbxassetid://2467567750"
+local animation2 = Instance.new("Animation")
+animation2.AnimationId = "rbxassetid://1957890538"
+local anims = {animation1, animation2}
 
-        local animation1 = Instance.new("Animation")
-        animation1.AnimationId = "rbxassetid://2467567750"
-        local animation2 = Instance.new("Animation")
-        animation2.AnimationId = "rbxassetid://1957890538"
-        local anims = {animation1, animation2}
+-- Play the animation continuously
+while wait(0.1) do
+    local an = lp.Character.Humanoid:LoadAnimation(anims[math.random(1, 2)])
+    an:Play()
+end
 
-        -- Play the animation continuously
-        while wait(0.1) do
-            local an = lp.Character.Humanoid:LoadAnimation(anims[math.random(1, 2)])
-            an:Play()
-        end
+local aa = Instance.new("Attachment")
+local ba = Instance.new("Attachment")
+local hinge = Instance.new("HingeConstraint", knife)
+hinge.Attachment0 = aa
+hinge.Attachment1 = ba
+hinge.LimitsEnabled = true
+hinge.LowerAngle = 0
+hinge.Restitution = 0
+hinge.UpperAngle = 0
 
-        local aa = Instance.new("Attachment")
-        local ba = Instance.new("Attachment")
-        local hinge = Instance.new("HingeConstraint", knife)
-        hinge.Attachment0 = aa
-        hinge.Attachment1 = ba
-        hinge.LimitsEnabled = true
-        hinge.LowerAngle = 0
-        hinge.Restitution = 0
-        hinge.UpperAngle = 0
-
-        for _, v in pairs(lp.Character:WaitForChild("UpperTorso"):GetChildren()) do
-            if v:IsA("Weld") and v.Part1 == knife then
-                v:Destroy()
-                break
-            end
-        end
-
-        game:GetService("RunService").Heartbeat:Connect(function()
-            setsimulationradius(1 / 0, 1 / 0)
-            if lp.Character and knife then
-                knife.CFrame = lp.Character:WaitForChild("UpperTorso").CFrame * CFrame.new(-0.200027466, -0.399999619, 0.5, 3.22982669e-05, -0.707153201, 0.707060337, 1.33886933e-05, 0.707060337, 0.707153141, -1, -1.33812428e-05, 3.22982669e-05)
-            end
-        end)
-        
-        knifeFunctionalityEnabled = true
-    else
-        -- Disable knife functionality
-        if knifeFunctionalityEnabled then
-            -- Remove knife constraints
-            for _, constraint in pairs(knife:GetChildren()) do
-                constraint:Destroy()
-            end
-            -- Stop animations
-            lp.Character.Humanoid:StopAllAnimations()
-            knifeFunctionalityEnabled = false
-        end
+for _, v in pairs(lp.Character:WaitForChild("UpperTorso"):GetChildren()) do
+    if v:IsA("Weld") and v.Part1 == knife then
+        v:Destroy()
+        break
     end
 end
 
-ToggleKnife:OnChanged(function(enabled)
-    print("Knife Toggle changed:", enabled)
-    ToggleKnifeFunction(enabled)
+game:GetService("RunService").Heartbeat:Connect(function()
+    setsimulationradius(1 / 0, 1 / 0)
+    if lp.Character and knife then
+        knife.CFrame = lp.Character:WaitForChild("UpperTorso").CFrame * CFrame.new(-0.200027466, -0.399999619, 0.5, 3.22982669e-05, -0.707153201, 0.707060337, 1.33886933e-05, 0.707060337, 0.707153141, -1, -1.33812428e-05, 3.22982669e-05)
+    end
 end)
-
-ToggleKnifeFunction(false)
+end
+})
 
 
 
