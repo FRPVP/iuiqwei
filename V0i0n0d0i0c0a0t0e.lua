a8fbdf6a3490316941b41fce0dcfcebea7223f64
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-28 19:48:32 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -3388,7 +3388,7 @@ game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0,
 game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 6.331, (fehowtarget.Character.LeftUpperLeg), fehowtarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -1, 0))
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "test444", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "test33", Default = false })
 
 Toggle:OnChanged(function(fehow)
     if fehow == true then
