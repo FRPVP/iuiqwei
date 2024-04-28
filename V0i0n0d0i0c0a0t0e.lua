5b933d8673e353621bdc2eb07e564db7ee3391ef
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-27 20:11:08 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -3512,6 +3512,47 @@ Options.MyToggle:SetValue(false)
 	
 
 
+function communistplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(6978648716, Enum.NormalId.Front, 5, (communistplayertarget.Character.HumanoidRootPart), communistplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,5,0))
+end
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Communist", Default = false })
+
+Toggle:OnChanged(function(communistplayer)
+    if communistplayer == true then
+        communistplayerloop = true
+        while communistplayerloop do
+            function communistplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        communistplayertarget = players:FindFirstChild(v.Name)
+                        communistplayerfunc()
+                        task.wait()
+                    end
+                else
+                    communistplayertarget = players:FindFirstChild(fetargetname)
+                    communistplayerfunc()
+                end
+                task.wait(15)
+            end
+            wait()
+            pcall(communistplayerloopfix)
+        end
+    end
+    if communistplayer == false then
+        communistplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
+
+
+
+
+
 
 
 
@@ -3556,43 +3597,6 @@ end)
 Options.MyToggle:SetValue(false)
 
 
-
-
-
-
-
-function funnyplayerfunc(funnyplayertarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10193511157, Enum.NormalId.Front, 1.5, (funnyplayertarget.Character.HumanoidRootPart), funnyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,1.2,-1))
-end
-
-Tabs.Premium:AddButton({
-    Title = "Troll Face (15sec)",
-    Description = "",
-    Icon = "", -- Replace "icon.png" with the path to your icon file
-    Callback = function()
-        -- Check if a name is selected
-        if fetargetname == "All" then
-            EquipSpray() -- Equip the spray first
-            -- Iterate over all players and execute funnyplayerfunc on each player except yourself
-            for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-                if player ~= game:GetService("Players").LocalPlayer then
-                    funnyplayerfunc(player)
-                end
-            end
-        elseif fetargetname ~= "" then
-            EquipSpray() -- Equip the spray first
-            -- Find the player with the selected name
-            local funnyplayertarget = game:GetService("Players"):FindFirstChild(fetargetname)
-            if funnyplayertarget then
-                funnyplayerfunc(funnyplayertarget) -- Execute funnyplayerfunc on the player
-            else
-                print("Player not found.")
-            end
-        else
-            print("Please select a name from the dropdown.")
-        end
-    end
-})
 
 
 
@@ -3738,48 +3742,6 @@ Toggle:OnChanged(function(spongeplayer)
     end
     if spongeplayer == false then
         spongeplayerloop = false
-        wait()
-    end
-end)
-
-Options.MyToggle:SetValue(false)
-
-
-
-
-
-
-function communistplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(6978648716, Enum.NormalId.Front, 5, (communistplayertarget.Character.HumanoidRootPart), communistplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,5,0))
-end
-
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Communist", Default = false })
-
-Toggle:OnChanged(function(communistplayer)
-    if communistplayer == true then
-        communistplayerloop = true
-        while communistplayerloop do
-            function communistplayerloopfix()
-                EquipSpray()
-                task.wait(0.4)
-                if fetargetname == "All" then
-                    for _, v in pairs(players:GetPlayers()) do
-                        communistplayertarget = players:FindFirstChild(v.Name)
-                        communistplayerfunc()
-                        task.wait()
-                    end
-                else
-                    communistplayertarget = players:FindFirstChild(fetargetname)
-                    communistplayerfunc()
-                end
-                task.wait(15)
-            end
-            wait()
-            pcall(communistplayerloopfix)
-        end
-    end
-    if communistplayer == false then
-        communistplayerloop = false
         wait()
     end
 end)
