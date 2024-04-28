1d7f852273debd36a8d70456ecda8f85dd111c2a
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-27 22:33:21 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,36 +1,39 @@
-function resetplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (resetplayertarget.Character.Head), resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+function feslipfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (fesliptarget.Character.LeftUpperLeg), fesliptarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (fesliptarget.Character.LeftUpperLeg), fesliptarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(7199393435, Enum.NormalId.Top, 3.5, (fesliptarget.Character.HumanoidRootPart), fesliptarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -3, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(7199393435, Enum.NormalId.Bottom, 3.5, (fesliptarget.Character.HumanoidRootPart), fesliptarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -2.85, 0))
 end
 
 local Toggle = Tabs.Premium:AddToggle("", {Title = "Loop Reset", Default = false })
 
-Toggle:OnChanged(function(resetplayer)
-    if resetplayer == true then
-        resetplayerloop = true
-        while resetplayerloop do
-            function resetplayerloopfix()
+Toggle:OnChanged(function(feslip)
+    if feslip == true then
+        fesliploop = true
+        while fesliploop do
+            function fesliploopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if fetargetname == "All" then
                     for _, v in pairs(players:GetPlayers()) do
                         if v ~= players.LocalPlayer then -- Skip executing the function on yourself
-                            resetplayertarget = players:FindFirstChild(v.Name)
-                            resetplayerfunc()
+                            fesliptarget = players:FindFirstChild(v.Name)
+                            feslipfunc()
                             task.wait()
                         end
                     end
                 else
-                    resetplayertarget = players:FindFirstChild(fetargetname)
-                    resetplayerfunc()
+                    fesliptarget = players:FindFirstChild(fetargetname)
+                    feslipfunc()
                 end
                 task.wait(0)
             end
             wait()
-            pcall(resetplayerloopfix)
+            pcall(fesliploopfix)
         end
     end
-    if resetplayer == false then
-        resetplayerloop = false
+    if feslip == false then
+        fesliploop = false
         wait()
     end
 end)
