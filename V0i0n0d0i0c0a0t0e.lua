4564378be8f244dbb91fa63d6d71be1675f5894c
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-30 16:02:26 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -3383,45 +3383,6 @@ Options.MyToggle:SetValue(false)
 
 
 
-function fehowfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (fehowtarget.Character.LeftHand), fehowtarget.Character.LeftHand.CFrame * CFrame.new(0, -2, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (fehowtarget.Character.LeftFoot), fehowtarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
-end
-
-local Toggle = Tabs.Premium:AddToggle("", {Title = "test8888", Default = false })
-
-Toggle:OnChanged(function(fehow)
-    if fehow == true then
-        fehowloop = true
-        while fehowloop do
-            function fehowloopfix()
-                EquipSpray()
-                task.wait(0.4)
-                if fetargetname == "All" then
-                    for _, v in pairs(players:GetPlayers()) do
-                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
-                            fehowtarget = players:FindFirstChild(v.Name)
-                            fehowfunc()
-                            task.wait()
-                        end
-                    end
-                else
-                    fehowtarget = players:FindFirstChild(fetargetname)
-                    fehowfunc()
-                end
-                task.wait(15)
-            end
-            wait()
-            pcall(fehowloopfix)
-        end
-    end
-    if fehow == false then
-        fehowloop = false
-        wait()
-    end
-end)
-
-Options.MyToggle:SetValue(false)
 
 	
 	
