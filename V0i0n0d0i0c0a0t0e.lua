9329352bc4a59fd1c3e70db6c6e89e9e45a048df
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-30 20:24:10 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -3514,6 +3514,12 @@ end)
 Options.MyToggle:SetValue(false)
 
 
+
+
+
+
+
+
 function feblackpenisfunc()
     -- Top Penis
     game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(60484593, Enum.NormalId.Top, 0.5, (feblackpenistarget.Character.HumanoidRootPart), feblackpenistarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,-1,-0.7))
@@ -3660,6 +3666,57 @@ Toggle:OnChanged(function(febigpenis)
 end)
 
 Options.MyToggle:SetValue(false)
+
+
+
+
+
+
+
+
+
+
+
+function cumplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(14976845153, Enum.NormalId.Front, 1.5, (cumplayertarget.Character.HumanoidRootPart), cumplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,-1.15,-2.90))
+end
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Cum On Penis", Default = false })
+
+Toggle:OnChanged(function(cumplayer)
+    if cumplayer == true then
+        cumplayerloop = true
+        while cumplayerloop do
+            function cumplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if infinityGauntlet == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            cumplayertarget = players:FindFirstChild(v.Name)
+                            cumplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    cumplayertarget = players:FindFirstChild(infinityGauntlet)
+                    cumplayerfunc()
+                end
+                task.wait(15)
+            end
+            wait()
+            pcall(cumplayerloopfix)
+        end
+    end
+    if cumplayer == false then
+        cumplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
+
+
 
 
 
