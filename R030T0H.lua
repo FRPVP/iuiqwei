3e243fd76eee686f1f9d93649b620ffd8af36207
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-30 20:22:57 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,36 +1,36 @@
-function laughplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(12877195347, Enum.NormalId.Front, 1.5, (laughplayertarget.Character.HumanoidRootPart), laughplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,1.2,-1))
+function cumplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(14976845153, Enum.NormalId.Front, 1.5, (cumplayertarget.Character.HumanoidRootPart), cumplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,-1.15,-2.90))
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "LOL", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Cum On Penis", Default = false })
 
-Toggle:OnChanged(function(laughplayer)
-    if laughplayer == true then
-        laughplayerloop = true
-        while laughplayerloop do
-            function laughplayerloopfix()
+Toggle:OnChanged(function(cumplayer)
+    if cumplayer == true then
+        cumplayerloop = true
+        while cumplayerloop do
+            function cumplayerloopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if infinityGauntlet == "All" then
                     for _, v in pairs(players:GetPlayers()) do
                         if v ~= players.LocalPlayer then -- Skip executing the function on yourself
-                            laughplayertarget = players:FindFirstChild(v.Name)
-                            laughplayerfunc()
+                            cumplayertarget = players:FindFirstChild(v.Name)
+                            cumplayerfunc()
                             task.wait()
                         end
                     end
                 else
-                    laughplayertarget = players:FindFirstChild(infinityGauntlet)
-                    laughplayerfunc()
+                    cumplayertarget = players:FindFirstChild(infinityGauntlet)
+                    cumplayerfunc()
                 end
                 task.wait(15)
             end
             wait()
-            pcall(laughplayerloopfix)
+            pcall(cumplayerloopfix)
         end
     end
-    if laughplayer == false then
-        laughplayerloop = false
+    if cumplayer == false then
+        cumplayerloop = false
         wait()
     end
 end)
