4b3f5cfe4b0d6f1c223ed4d65cad3d38be612c2c
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-04-30 20:34:41 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,34 +1,34 @@
-function cumplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(14976845153, Enum.NormalId.Front, 1.5, (cumplayertarget.Character.HumanoidRootPart), cumplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,-1.15,-2.90))
+function laughplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(12877195347, Enum.NormalId.Front, 1.5, (laughplayertarget.Character.HumanoidRootPart), laughplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,1.2,-1))
 end
 
 local Toggle = Tabs.Premium:AddToggle("", {Title = "Cum on Penis", Default = false })
 
-Toggle:OnChanged(function(cumplayer)
-    if cumplayer == true then
-        cumplayerloop = true
-        while cumplayerloop do
-            function cumplayerloopfix()
+Toggle:OnChanged(function(laughplayer)
+    if laughplayer == true then
+        laughplayerloop = true
+        while laughplayerloop do
+            function laughplayerloopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if infinityGauntlet == "All" then
                     for _, v in pairs(players:GetPlayers()) do
-                        cumplayertarget = players:FindFirstChild(v.Name)
-                        cumplayerfunc()
+                        laughplayertarget = players:FindFirstChild(v.Name)
+                        laughplayerfunc()
                         task.wait()
                     end
                 else
-                    cumplayertarget = players:FindFirstChild(infinityGauntlet)
-                    cumplayerfunc()
+                    laughplayertarget = players:FindFirstChild(infinityGauntlet)
+                    laughplayerfunc()
                 end
                 task.wait(15)
             end
             wait()
-            pcall(cumplayerloopfix)
+            pcall(laughplayerloopfix)
         end
     end
-    if cumplayer == false then
-        cumplayerloop = false
+    if laughplayer == false then
+        laughplayerloop = false
         wait()
     end
 end)
