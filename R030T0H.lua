ea9d90bfdfee4595b001082ea3c35aac44af2151
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-03 17:27:02 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,45 +1,45 @@
-function defeatplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Top, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Bottom, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3.15, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Bottom, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -2.8, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Top, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -3, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Front, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 3.1))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Back, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 2.86))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Front, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -2.86))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Back, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -3.1))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Right, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(2.86, 0.1, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Left, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(3.1, 0.1, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Right, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373315144, Enum.NormalId.Left, 6, (defeatplayertarget.Character.HumanoidRootPart), defeatplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
+function isuadyplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Top, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Bottom, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3.15, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Bottom, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -2.8, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Top, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -3, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Front, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 3.1))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Back, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 2.86))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Front, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -2.86))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Back, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -3.1))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Right, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(2.86, 0.1, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Left, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(3.1, 0.1, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Right, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Left, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Spongebob Sex 3", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Spongebob Sex 4", Default = false })
 
-Toggle:OnChanged(function(defeatplayer)
-    if defeatplayer == true then
-        defeatplayerloop = true
-        while defeatplayerloop do
-            function defeatplayerloopfix()
+Toggle:OnChanged(function(isuadyplayer)
+    if isuadyplayer == true then
+        isuadyplayerloop = true
+        while isuadyplayerloop do
+            function isuadyplayerloopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if infinityGauntlet == "All" then
                     for _, v in pairs(players:GetPlayers()) do
-                        defeatplayertarget = players:FindFirstChild(v.Name)
-                        defeatplayerfunc()
+                        isuadyplayertarget = players:FindFirstChild(v.Name)
+                        isuadyplayerfunc()
                         task.wait()
                     end
                 else
-                    defeatplayertarget = players:FindFirstChild(infinityGauntlet)
-                    defeatplayerfunc()
+                    isuadyplayertarget = players:FindFirstChild(infinityGauntlet)
+                    isuadyplayerfunc()
                 end
                 task.wait(15)
             end
             wait()
-            pcall(defeatplayerloopfix)
+            pcall(isuadyplayerloopfix)
         end
     end
-    if defeatplayer == false then
-        defeatplayerloop = false
+    if isuadyplayer == false then
+        isuadyplayerloop = false
         wait()
     end
 end)
