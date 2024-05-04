6620d2f418055b83550e24ea194336e278d3a2c0
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-03 20:26:39 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,45 +1,34 @@
-function isuadyplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Top, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Bottom, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3.15, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Bottom, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -2.8, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Top, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -3, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Front, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 3.1))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Back, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 2.86))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Front, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -2.86))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Back, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -3.1))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Right, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(2.86, 0.1, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Left, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(3.1, 0.1, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Right, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17373265789, Enum.NormalId.Left, 6, (isuadyplayertarget.Character.HumanoidRootPart), isuadyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
+function phubplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17189770364, Enum.NormalId.Front, 5, (phubplayertarget.Character.HumanoidRootPart), phubplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,5,0))
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Spongebob Sex 4", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "PHub Logo", Default = false })
 
-Toggle:OnChanged(function(isuadyplayer)
-    if isuadyplayer == true then
-        isuadyplayerloop = true
-        while isuadyplayerloop do
-            function isuadyplayerloopfix()
+Toggle:OnChanged(function(phubplayer)
+    if phubplayer == true then
+        phubplayerloop = true
+        while phubplayerloop do
+            function phubplayerloopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if infinityGauntlet == "All" then
                     for _, v in pairs(players:GetPlayers()) do
-                        isuadyplayertarget = players:FindFirstChild(v.Name)
-                        isuadyplayerfunc()
+                        phubplayertarget = players:FindFirstChild(v.Name)
+                        phubplayerfunc()
                         task.wait()
                     end
                 else
-                    isuadyplayertarget = players:FindFirstChild(infinityGauntlet)
-                    isuadyplayerfunc()
+                    phubplayertarget = players:FindFirstChild(infinityGauntlet)
+                    phubplayerfunc()
                 end
                 task.wait(15)
             end
             wait()
-            pcall(isuadyplayerloopfix)
+            pcall(phubplayerloopfix)
         end
     end
-    if isuadyplayer == false then
-        isuadyplayerloop = false
+    if phubplayer == false then
+        phubplayerloop = false
         wait()
     end
 end)
