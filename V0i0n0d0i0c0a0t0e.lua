0757917200f8958ff8884e2dc6f7deba5bd7e49d
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-03 20:27:22 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -4589,6 +4589,51 @@ Options.MyToggle:SetValue(false)
 
 
 
+function phubplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(17189770364, Enum.NormalId.Front, 5, (phubplayertarget.Character.HumanoidRootPart), phubplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,5,0))
+end
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "PHub Logo", Default = false })
+
+Toggle:OnChanged(function(phubplayer)
+    if phubplayer == true then
+        phubplayerloop = true
+        while phubplayerloop do
+            function phubplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if infinityGauntlet == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        phubplayertarget = players:FindFirstChild(v.Name)
+                        phubplayerfunc()
+                        task.wait()
+                    end
+                else
+                    phubplayertarget = players:FindFirstChild(infinityGauntlet)
+                    phubplayerfunc()
+                end
+                task.wait(15)
+            end
+            wait()
+            pcall(phubplayerloopfix)
+        end
+    end
+    if phubplayer == false then
+        phubplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
+
+
+
+
+
+
+
+
+
 
 
 local section = Tabs.Trolling:AddSection("Fling")
