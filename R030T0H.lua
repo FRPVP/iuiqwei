37d6cb3f81583433f1361b4dd78a913c639024a0
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-05 19:54:48 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,59 +1,35 @@
-function missmeplayerfunc()
-    local box1 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Top, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3, 0))
-    local box2 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Bottom, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 3.15, 0))
-    local box3 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Bottom, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -2.8, 0))
-    local box4 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Top, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -3, 0))
-    local box5 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Front, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 3.1))
-    local box6 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Back, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, 2.86))
-    local box7 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Front, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -2.86))
-    local box8 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Back, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0.1, -3.1))
-    local box9 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Right, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(2.86, 0.1, 0))
-    local box10 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Left, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(3.1, 0.1, 0))
-    local box11 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Right, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
-    local box12 = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(10180722469, Enum.NormalId.Left, 6, (missmeplayertarget.Character.HumanoidRootPart), missmeplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
-    
-    -- Unanchoring the boxes
-    box1.Anchored = false
-    box2.Anchored = false
-    box3.Anchored = false
-    box4.Anchored = false
-    box5.Anchored = false
-    box6.Anchored = false
-    box7.Anchored = false
-    box8.Anchored = false
-    box9.Anchored = false
-    box10.Anchored = false
-    box11.Anchored = false
-    box12.Anchored = false
+function filedownplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(15093138669, Enum.NormalId.Right, 3, (filedownplayertarget.Character.RightHand), filedownplayertarget.Character.RightHand.CFrame * CFrame.new(0, 0, -0.7))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(15096522641, Enum.NormalId.Left, 3, (filedownplayertarget.Character.RightHand), filedownplayertarget.Character.RightHand.CFrame * CFrame.new(0, 0, -0.7))
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Rickroll", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Fake Knife", Default = false })
 
-Toggle:OnChanged(function(missmeplayer)
-    if missmeplayer == true then
-        missmeplayerloop = true
-        while missmeplayerloop do
-            function missmeplayerloopfix()
+Toggle:OnChanged(function(filedownplayer)
+    if filedownplayer == true then
+        filedownplayerloop = true
+        while filedownplayerloop do
+            function filedownplayerloopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if infinityGauntlet == "All" then
                     for _, v in pairs(players:GetPlayers()) do
-                        missmeplayertarget = players:FindFirstChild(v.Name)
-                        missmeplayerfunc()
+                        filedownplayertarget = players:FindFirstChild(v.Name)
+                        filedownplayerfunc()
                         task.wait()
                     end
                 else
-                    missmeplayertarget = players:FindFirstChild(infinityGauntlet)
-                    missmeplayerfunc()
+                    filedownplayertarget = players:FindFirstChild(infinityGauntlet)
+                    filedownplayerfunc()
                 end
                 task.wait(15)
             end
             wait()
-            pcall(missmeplayerloopfix)
+            pcall(filedownplayerloopfix)
         end
     end
-    if missmeplayer == false then
-        missmeplayerloop = false
+    if filedownplayer == false then
+        filedownplayerloop = false
         wait()
     end
 end)
