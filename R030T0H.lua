35533103092ada7d3198934d25efb93b2c6602ed
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-05 17:34:04 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,42 +1,37 @@
-function cantreadfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (cantreadtarget.Character.RightUpperLeg), cantreadtarget.Character.RightUpperLeg.CFrame * CFrame.new(0, 0, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (cantreadtarget.Character.LeftUpperLeg), cantreadtarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, 0, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (cantreadtarget.Character.HumanoidRootPart), cantreadtarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Bottom, 6.331, (cantreadtarget.Character.LeftHand), cantreadtarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (cantreadtarget.Character.RightLowerLeg), cantreadtarget.Character.RightLowerLeg.CFrame * CFrame.new(0, 0, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (cantreadtarget.Character.LeftLowerLeg), cantreadtarget.Character.LeftLowerLeg.CFrame * CFrame.new(0, 0, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (cantreadtarget.Character.Head), cantreadtarget.Character.Head.CFrame * CFrame.new(0, 0, 0))
+function feslipfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(7199393435, Enum.NormalId.Top, 3.5, (fesliptarget.Character.HumanoidRootPart), fesliptarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -3, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(7199393435, Enum.NormalId.Bottom, 3.5, (fesliptarget.Character.HumanoidRootPart), fesliptarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, -2.85, 0))
 end
 
 local Toggle = Tabs.Premium:AddToggle("", {Title = "test", Default = false })
 
-Toggle:OnChanged(function(cantread)
-    if cantread == true then
-        cantreadloop = true
-        while cantreadloop do
-            function cantreadloopfix()
+Toggle:OnChanged(function(feslip)
+    if feslip == true then
+        fesliploop = true
+        while fesliploop do
+            function fesliploopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if fetargetname == "All" then
                     for _, v in pairs(players:GetPlayers()) do
                         if v ~= players.LocalPlayer then -- Skip executing the function on yourself
-                            cantreadtarget = players:FindFirstChild(v.Name)
-                            cantreadfunc()
+                            fesliptarget = players:FindFirstChild(v.Name)
+                            feslipfunc()
                             task.wait()
                         end
                     end
                 else
-                    cantreadtarget = players:FindFirstChild(fetargetname)
-                    cantreadfunc()
+                    fesliptarget = players:FindFirstChild(fetargetname)
+                    feslipfunc()
                 end
                 task.wait(15)
             end
             wait()
-            pcall(cantreadloopfix)
+            pcall(fesliploopfix)
         end
     end
-    if cantread == false then
-        cantreadloop = false
+    if feslip == false then
+        fesliploop = false
         wait()
     end
 end)
