ccc6be3a383f10e29fcf2e85079f1e12dfd977d3
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-06 17:58:55 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,33 +1,39 @@
-function aiwudnplayerfunc(aiwudnplayertarget)
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (aiwudnplayertarget.Character.HumanoidRootPart), aiwudnplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (aiwudnplayertarget.Character.RightHand), aiwudnplayertarget.Character.RightHand.CFrame * CFrame.new(0, 99999, 0))
+function iawudauyyplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (iawudauyyplayertarget.Character.HumanoidRootPart), iawudauyyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (iawudauyyplayertarget.Character.RightHand), iawudauyyplayertarget.Character.RightHand.CFrame * CFrame.new(0, 99999, 0))
 end
 
-Tabs.Premium:AddButton({
-    Title = "testing",
-    Description = "",
-    Icon = "", -- Replace "icon.png" with the path to your icon file
-    Callback = function()
-        -- Check if a name is selected
-        if fetargetname == "All" then
-            EquipSpray() -- Equip the spray first
-            -- Iterate over all players and execute aiwudnplayerfunc on each player except yourself
-            for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-                if player ~= game:GetService("Players").LocalPlayer then
-                    aiwudnplayerfunc(player)
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Testing", Default = false })
+
+Toggle:OnChanged(function(iawudauyyplayer)
+    if iawudauyyplayer == true then
+        iawudauyyplayerloop = true
+        while iawudauyyplayerloop do
+            function iawudauyyplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            iawudauyyplayertarget = players:FindFirstChild(v.Name)
+                            iawudauyyplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    iawudauyyplayertarget = players:FindFirstChild(fetargetname)
+                    iawudauyyplayerfunc()
                 end
+                task.wait(0)
             end
-        elseif fetargetname ~= "" then
-            EquipSpray() -- Equip the spray first
-            -- Find the player with the selected name
-            local aiwudnplayertarget = game:GetService("Players"):FindFirstChild(fetargetname)
-            if aiwudnplayertarget then
-                aiwudnplayerfunc(aiwudnplayertarget) -- Execute aiwudnplayerfunc on the player
-            else
-                print("Player not found.")
-            end
-        else
-            print("Please select a name from the dropdown.")
+            wait()
+            pcall(iawudauyyplayerloopfix)
         end
     end
-})
+    if iawudauyyplayer == false then
+        iawudauyyplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
