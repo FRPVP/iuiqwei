58bb17257e8f22982f3b67681aaa09dc2da83324
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-06 21:25:25 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -3795,6 +3795,48 @@ Options.MyToggle:SetValue(false)
 
 
 
+function iawudauyyplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (iawudauyyplayertarget.Character.HumanoidRootPart), iawudauyyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Bottom, 6.331, (iawudauyyplayertarget.Character.LeftHand), iawudauyyplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 80000, 0))
+end
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Void", Default = false })
+
+Toggle:OnChanged(function(iawudauyyplayer)
+    if iawudauyyplayer == true then
+        iawudauyyplayerloop = true
+        while iawudauyyplayerloop do
+            function iawudauyyplayerloopfix()
+                EquipSpray()
+                task.wait(0.4)
+                if fetargetname == "All" then
+                    for _, v in pairs(players:GetPlayers()) do
+                        if v ~= players.LocalPlayer then -- Skip executing the function on yourself
+                            iawudauyyplayertarget = players:FindFirstChild(v.Name)
+                            iawudauyyplayerfunc()
+                            task.wait()
+                        end
+                    end
+                else
+                    iawudauyyplayertarget = players:FindFirstChild(fetargetname)
+                    iawudauyyplayerfunc()
+                end
+                task.wait(0)
+            end
+            wait()
+            pcall(iawudauyyplayerloopfix)
+        end
+    end
+    if iawudauyyplayer == false then
+        iawudauyyplayerloop = false
+        wait()
+    end
+end)
+
+Options.MyToggle:SetValue(false)
+
+
+
 
 
 
