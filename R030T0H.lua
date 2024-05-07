a8d4ca045820db6471c308bc8418c1adb801d12b
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-07 18:18:51 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,37 +1,38 @@
-function iawudauyyplayerfunc()
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (iawudauyyplayertarget.Character.HumanoidRootPart), iawudauyyplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (iawudauyyplayertarget.Character.RightHand), iawudauyyplayertarget.Character.RightHand.CFrame * CFrame.new(0, 99999, 0))
+function fghhttyklplayerfunc()
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, (fghhttyklplayertarget.Character.HumanoidRootPart), fghhttyklplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 99999))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (fghhttyklplayertarget.Character.LeftUpperLeg), fghhttyklplayertarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
+game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, (fghhttyklplayertarget.Character.LeftUpperLeg), fghhttyklplayertarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
 end
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Testing", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Void", Default = false })
 
-Toggle:OnChanged(function(iawudauyyplayer)
-    if iawudauyyplayer == true then
-        iawudauyyplayerloop = true
-        while iawudauyyplayerloop do
-            function iawudauyyplayerloopfix()
+Toggle:OnChanged(function(fghhttyklplayer)
+    if fghhttyklplayer == true then
+        fghhttyklplayerloop = true
+        while fghhttyklplayerloop do
+            function fghhttyklplayerloopfix()
                 EquipSpray()
                 task.wait(0.4)
                 if fetargetname == "All" then
                     for _, v in pairs(players:GetPlayers()) do
                         if v ~= players.LocalPlayer then -- Skip executing the function on yourself
-                            iawudauyyplayertarget = players:FindFirstChild(v.Name)
-                            iawudauyyplayerfunc()
+                            fghhttyklplayertarget = players:FindFirstChild(v.Name)
+                            fghhttyklplayerfunc()
                             task.wait()
                         end
                     end
                 else
-                    iawudauyyplayertarget = players:FindFirstChild(fetargetname)
-                    iawudauyyplayerfunc()
+                    fghhttyklplayertarget = players:FindFirstChild(fetargetname)
+                    fghhttyklplayerfunc()
                 end
                 task.wait(0)
             end
             wait()
-            pcall(iawudauyyplayerloopfix)
+            pcall(fghhttyklplayerloopfix)
         end
     end
-    if iawudauyyplayer == false then
-        iawudauyyplayerloop = false
+    if fghhttyklplayer == false then
+        fghhttyklplayerloop = false
         wait()
     end
 end)
