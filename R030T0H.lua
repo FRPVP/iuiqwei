8ef6cd846d8c2fd4a8927f87eb1e9a3d622ea5ef
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-10 18:45:02 -0400

    Update R030T0H.lua

--- R030T0H.lua
+++ R030T0H.lua
@@ -1,12 +1,22 @@
-local function rickplayerfunc(sprayID, target)
+local function mxcvasdlplayerfunc(sprayID, target)
     local sprayRemote = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote
     local rootPart = target.Character.HumanoidRootPart
     sprayRemote:FireServer(sprayID, Enum.NormalId.Top, 6, rootPart, rootPart.CFrame * CFrame.new(0, 3, 0))
-    -- Repeat for other directions
+sprayRemote:FireServer(sprayID, Enum.NormalId.Bottom, 6, rootPart, rootPart.CFrame * CFrame.new(0, 3.15, 0))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Bottom, 6, rootPart, rootPart.CFrame * CFrame.new(0, -2.8, 0))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Top, 6, rootPart, rootPart.CFrame * CFrame.new(0, -3, 0))
+gsprayRemote:FireServer(sprayID, Enum.NormalId.Front, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, 3.1))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Back, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, 2.86))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Front, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, -2.86))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Back, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, -3.1))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Right, 6, rootPart, rootPart.CFrame * CFrame.new(2.86, 0.1, 0))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Left, 6, rootPart, rootPart.CFrame * CFrame.new(3.1, 0.1, 0))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Right, 6, rootPart, rootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
+sprayRemote:FireServer(sprayID, Enum.NormalId.Left, 6, rootPart, rootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
 end
 
-local rickplayerloop = false
-local sprayID = 10180722469 -- Default spray ID
+local mxcvasdlplayerloop = false
+local sprayID = 0 -- Default spray ID
 
 local Input = Tabs.Player:AddInput("Input", {
     Title = "Spray ID",
@@ -16,43 +26,41 @@ local Input = Tabs.Player:AddInput("Input", {
     Finished = true, -- Calls callback when you press enter
     Callback = function(Value)
         sprayID = tonumber(Value) or sprayID -- Convert input to number, or keep default if invalid
-        print("Spray ID changed:", sprayID)
     end
 })
 
 Input:OnChanged(function()
     sprayID = tonumber(Input.Value) or sprayID -- Convert input to number, or keep default if invalid
-    print("Spray ID updated:", sprayID)
 end)
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Rickroll", Default = false })
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Box Player", Default = false })
 
-Toggle:OnChanged(function(rickplayer)
-    if rickplayer then
-        rickplayerloop = true
-        while rickplayerloop do
+Toggle:OnChanged(function(mxcvasdlplayer)
+    if mxcvasdlplayer then
+        mxcvasdlplayerloop = true
+        while mxcvasdlplayerloop do
             EquipSpray()
             task.wait(0.4)
             local infinityGauntlet = Options.MyToggle:Value()
             local players = game:GetService("Players")
             if infinityGauntlet == "All" then
                 for _, v in pairs(players:GetPlayers()) do
-                    local rickplayertarget = players:FindFirstChild(v.Name)
-                    if rickplayertarget then
-                        rickplayerfunc(sprayID, rickplayertarget)
+                    local mxcvasdlplayertarget = players:FindFirstChild(v.Name)
+                    if mxcvasdlplayertarget then
+                        mxcvasdlplayerfunc(sprayID, mxcvasdlplayertarget)
                         task.wait()
                     end
                 end
             else
-                local rickplayertarget = players:FindFirstChild(infinityGauntlet)
-                if rickplayertarget then
-                    rickplayerfunc(sprayID, rickplayertarget)
+                local mxcvasdlplayertarget = players:FindFirstChild(infinityGauntlet)
+                if mxcvasdlplayertarget then
+                    mxcvasdlplayerfunc(sprayID, mxcvasdlplayertarget)
                 end
             end
             task.wait(15)
         end
     else
-        rickplayerloop = false
+        mxcvasdlplayerloop = false
     end
 end)
 
