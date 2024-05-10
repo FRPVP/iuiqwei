83b663fd845844ca74fb20ab6cd545931e47433f
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-10 18:57:59 -0400

    Update V0i0n0d0i0c0a0t0e.lua

--- V0i0n0d0i0c0a0t0e.lua
+++ V0i0n0d0i0c0a0t0e.lua
@@ -5144,69 +5144,72 @@ Options.MyToggle:SetValue(false)
 
 
 
-local function mxcvasdlplayerfunc(sprayID, target)
-    local sprayRemote = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote
-    local rootPart = target.Character.HumanoidRootPart
-    sprayRemote:FireServer(sprayID, Enum.NormalId.Top, 6, rootPart, rootPart.CFrame * CFrame.new(0, 3, 0))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Bottom, 6, rootPart, rootPart.CFrame * CFrame.new(0, 3.15, 0))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Bottom, 6, rootPart, rootPart.CFrame * CFrame.new(0, -2.8, 0))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Top, 6, rootPart, rootPart.CFrame * CFrame.new(0, -3, 0))
-gsprayRemote:FireServer(sprayID, Enum.NormalId.Front, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, 3.1))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Back, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, 2.86))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Front, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, -2.86))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Back, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, -3.1))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Right, 6, rootPart, rootPart.CFrame * CFrame.new(2.86, 0.1, 0))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Left, 6, rootPart, rootPart.CFrame * CFrame.new(3.1, 0.1, 0))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Right, 6, rootPart, rootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
-sprayRemote:FireServer(sprayID, Enum.NormalId.Left, 6, rootPart, rootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
-end
+local customID = "12781220539" -- Default ID
 
-local mxcvasdlplayerloop = false
-local sprayID = 12781220539 -- Default spray ID
-
-local Input = Tabs.Player:AddInput("Input", {
-    Title = "Spray ID",
-    Default = sprayID,
-    Placeholder = "Enter spray ID",
-    Numeric = true, -- Allows only numbers
+local Input = Tabs.Premium:AddInput("Input", {
+    Title = "Custom ID",
+    Default = customID,
+    Placeholder = "Enter custom ID",
+    Numeric = true, -- Only allows numbers
     Finished = true, -- Calls callback when you press enter
     Callback = function(Value)
-        sprayID = tonumber(Value) or sprayID -- Convert input to number, or keep default if invalid
+        print("Custom ID changed:", Value)
+        customID = Value
     end
 })
 
 Input:OnChanged(function()
-    sprayID = tonumber(Input.Value) or sprayID -- Convert input to number, or keep default if invalid
+    print("Custom ID updated:", Input.Value)
+    customID = Input.Value
 end)
 
-local Toggle = Tabs.Premium:AddToggle("", {Title = "Box Player", Default = false })
+function asdasxzcplayerfunc(id)
+    local sprayRemote = game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote
+    local rootPart = asdasxzcplayertarget.Character.HumanoidRootPart
+    sprayRemote:FireServer(id, Enum.NormalId.Top, 6, rootPart, rootPart.CFrame * CFrame.new(0, 3, 0))
+    sprayRemote:FireServer(id, Enum.NormalId.Bottom, 6, rootPart, rootPart.CFrame * CFrame.new(0, 3.15, 0))
+sprayRemote:FireServer(id, Enum.NormalId.Bottom, 6, rootPart, rootPart.CFrame * CFrame.new(0, -2.8, 0))
+sprayRemote:FireServer(id, Enum.NormalId.Top, 6, rootPart, rootPart.CFrame * CFrame.new(0, -3, 0))
+gsprayRemote:FireServer(id, Enum.NormalId.Front, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, 3.1))
+sprayRemote:FireServer(id, Enum.NormalId.Back, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, 2.86))
+sprayRemote:FireServer(id, Enum.NormalId.Front, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, -2.86))
+sprayRemote:FireServer(id, Enum.NormalId.Back, 6, rootPart, rootPart.CFrame * CFrame.new(0, 0.1, -3.1))
+sprayRemote:FireServer(id, Enum.NormalId.Right, 6, rootPart, rootPart.CFrame * CFrame.new(2.86, 0.1, 0))
+sprayRemote:FireServer(id, Enum.NormalId.Left, 6, rootPart, rootPart.CFrame * CFrame.new(3.1, 0.1, 0))
+sprayRemote:FireServer(id, Enum.NormalId.Right, 6, rootPart, rootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
+sprayRemote:FireServer(id, Enum.NormalId.Left, 6, rootPart, rootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
+end
 
-Toggle:OnChanged(function(mxcvasdlplayer)
-    if mxcvasdlplayer then
-        mxcvasdlplayerloop = true
-        while mxcvasdlplayerloop do
+local asdasxzcplayerloop = false
+
+local Toggle = Tabs.Premium:AddToggle("", {Title = "Box", Default = false })
+
+Toggle:OnChanged(function(asdasxzcplayer)
+    if asdasxzcplayer then
+        asdasxzcplayerloop = true
+        while asdasxzcplayerloop do
             EquipSpray()
             task.wait(0.4)
             local infinityGauntlet = Options.MyToggle:Value()
             local players = game:GetService("Players")
             if infinityGauntlet == "All" then
                 for _, v in pairs(players:GetPlayers()) do
-                    local mxcvasdlplayertarget = players:FindFirstChild(v.Name)
-                    if mxcvasdlplayertarget then
-                        mxcvasdlplayerfunc(sprayID, mxcvasdlplayertarget)
+                    local asdasxzcplayertarget = players:FindFirstChild(v.Name)
+                    if asdasxzcplayertarget then
+                        asdasxzcplayerfunc(customID)
                         task.wait()
                     end
                 end
             else
-                local mxcvasdlplayertarget = players:FindFirstChild(infinityGauntlet)
-                if mxcvasdlplayertarget then
-                    mxcvasdlplayerfunc(sprayID, mxcvasdlplayertarget)
+                local asdasxzcplayertarget = players:FindFirstChild(infinityGauntlet)
+                if asdasxzcplayertarget then
+                    asdasxzcplayerfunc(customID)
                 end
             end
             task.wait(15)
         end
     else
-        mxcvasdlplayerloop = false
+        asdasxzcplayerloop = false
     end
 end)
 
