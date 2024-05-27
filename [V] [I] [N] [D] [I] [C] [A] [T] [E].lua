69f1719490b36b0685f4ed524cadc078be52829d
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-27 19:50:20 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -2169,6 +2169,45 @@ tab:toggle({
     end
 end,})
 
+function andlxcaosplayerfunc(andlxcaosplayertarget)
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Right, 2048, andlxcaosplayertarget.Character.HumanoidRootPart, andlxcaosplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 250, 0))
+end
+
+local andlxcaosplayerloop = false
+
+tab:toggle({
+    Name = "Lag",
+		StartingState = false,
+		Description = "Spraypaint Toy Required",
+		Callback = function(Value)
+   if Value == true then
+        andlxcaosplayerloop = true
+        while andlxcaosplayerloop do
+            EquipSpray()
+            task.wait(0.4)
+            if fetargetname == "All" then
+                for _, v in pairs(game.Players:GetPlayers()) do
+                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
+                        local andlxcaosplayertarget = v
+                        andlxcaosplayerfunc(andlxcaosplayertarget)
+                        task.wait()
+                    end
+                end
+            else
+                local andlxcaosplayertarget = findPlayerByName(fetargetname)
+                if andlxcaosplayertarget then
+                    andlxcaosplayerfunc(andlxcaosplayertarget)
+                else
+                    print("Player not found.")
+                end
+            end
+            task.wait(0)
+        end
+    end
+    if Value == false then
+        andlxcaosplayerloop = false
+    end
+end,})
 
 
 
