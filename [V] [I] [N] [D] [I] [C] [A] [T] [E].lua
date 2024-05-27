9fabe8472fca4d00084caa73c4116e3477d73584
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-27 00:14:53 -0400

    Create [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

---
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -0,0 +1,50 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FRPVP/iuiqwei/main/library.lua"))()
+
+local gui = Library:create{
+    Theme = Library.Themes.Dark
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628898821",
+    Name = "Player"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628884815",
+    Name = "Visual"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628895810",
+    Name = "Combat"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628887332",
+    Name = "Teleport"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628882295",
+    Name = "Trolling"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628879224",
+    Name = "Autofarm"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628876487",
+    Name = "Emotes"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17628892018",
+    Name = "Spraypaint"
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://17629022447",
+    Name = "Misc"
+}
