3f914cfccf3ec066a0429df30549408d84233ace
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-27 19:06:29 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -2160,7 +2160,45 @@ tab:toggle({
     end
 end,})
 
+function jadoiwanplayerfunc(jadoiwanplayertarget)
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, jadoiwanplayertarget.Character.LeftHand, jadoiwanplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
+end
 
+local jadoiwanplayerloop = false
+
+tab:toggle({
+    Name = "Freeze",
+		StartingState = false,
+		Description = "Spraypaint Toy Required",
+		Callback = function(Value)
+   if Value == true then
+        jadoiwanplayerloop = true
+        while jadoiwanplayerloop do
+            EquipSpray()
+            task.wait(0.4)
+            if fetargetname == "All" then
+                for _, v in pairs(game.Players:GetPlayers()) do
+                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
+                        local jadoiwanplayertarget = v
+                        jadoiwanplayerfunc(jadoiwanplayertarget)
+                        task.wait()
+                    end
+                end
+            else
+                local jadoiwanplayertarget = findPlayerByName(fetargetname)
+                if jadoiwanplayertarget then
+                    jadoiwanplayerfunc(jadoiwanplayertarget)
+                else
+                    print("Player not found.")
+                end
+            end
+            task.wait(0)
+        end
+    end
+    if Value == false then
+        jadoiwanplayerloop = false
+    end
+end,})
 
 
 
