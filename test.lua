71c4ff765c6441de3f5e4c0054c01b2ac19d630c
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-28 15:08:08 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,87 +1,96 @@
 local Players = game:GetService("Players")
-local bigpenisId = 60484593
+local sprayId = 60484593
 
-function xnmcvzxkcjaaaplayerfunc(xnmcvzxkcjaaaplayertarget)
+local function sprayOnPlayer(target)
     local localPlayer = Players.LocalPlayer
-    local target = xnmcvzxkcjaaaplayertarget
-    local humanoidRootPart = target.Character.HumanoidRootPart
+    if not target.Character or not target.Character:FindFirstChild("HumanoidRootPart") then
+        return
+    end
     
-    -- Top Penis
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -0.7))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -1))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -1.5))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -2))
+    local humanoidRootPart = target.Character.HumanoidRootPart
+
+    -- Top Spray
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -0.7))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -1))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -1.5))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -2))
     localPlayer.Character.SprayPaint.Remote:FireServer(13850207336, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1, -2.5))
 
-    -- Bottom Penis
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -0.5))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -1))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -1.5))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -2))
+    -- Bottom Spray
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -0.5))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -1))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -1.5))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -2))
     localPlayer.Character.SprayPaint.Remote:FireServer(13850207336, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.3, -2.5))
 
-    -- Left Penis
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -0.5))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -1))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -1.5))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -2))
+    -- Left Spray
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -0.5))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -1))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -1.5))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -2))
     localPlayer.Character.SprayPaint.Remote:FireServer(13850207336, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.15, -1.15, -2.5))
 
-    -- Right Penis
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -0.5))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -1))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -1.5))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -2))
+    -- Right Spray
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -0.5))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -1))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -1.5))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -2))
     localPlayer.Character.SprayPaint.Remote:FireServer(13850207336, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.15, -1.15, -2.5))
 
-    -- Front Penis
+    -- Front Spray
     localPlayer.Character.SprayPaint.Remote:FireServer(13850207336, Enum.NormalId.Front, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -1.15, -2.65))
 
     -- Left Sack
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.5, -1, -0.7))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Front, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.5, -1.15, -0.85))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.5, -1.3, -0.7))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.65, -1.15, -0.7))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.5, -1, -0.7))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Front, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.5, -1.15, -0.85))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.5, -1.3, -0.7))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Right, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0.65, -1.15, -0.7))
 
     -- Right Sack
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.5, -1, -0.7))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Front, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.5, -1.15, -0.85))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.5, -1.3, -0.7))
-    localPlayer.Character.SprayPaint.Remote:FireServer(penisId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.65, -1.15, -0.7))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Top, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.5, -1, -0.7))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Front, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.5, -1.15, -0.85))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Bottom, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.5, -1.3, -0.7))
+    localPlayer.Character.SprayPaint.Remote:FireServer(sprayId, Enum.NormalId.Left, 0.5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-0.65, -1.15, -0.7))
 end
 
-local xnmcvzxkcjaaaplayerloop = false
+local sprayLoop = false
 
 tab:toggle({
     Name = "Penis",
-		StartingState = false,
-		Description = "Spraypaint Toy Required",
-		Callback = function(Value)
-   if Value == true then
-        xnmcvzxkcjaaaplayerloop = true
-        while xnmcvzxkcjaaaplayerloop do
-            EquipSpray()
-            task.wait(0.4)
-            if infinityGauntlet == "All" then
-                for _, v in pairs(game.Players:GetPlayers()) do
-                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local xnmcvzxkcjaaaplayertarget = v
-                        xnmcvzxkcjaaaplayerfunc(xnmcvzxkcjaaaplayertarget)
-                        task.wait()
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(Value)
+        if Value then
+            sprayLoop = true
+            while sprayLoop do
+                EquipSpray()
+                task.wait(0.4)
+                if infinityGauntlet == "All" then
+                    for _, player in pairs(game.Players:GetPlayers()) do
+                        if player ~= Players.LocalPlayer then -- Skip executing the function on yourself
+                            sprayOnPlayer(player)
+                            task.wait()
+                        end
+                    end
+                else
+                    local targetPlayer = findPlayerByName(infinityGauntlet)
+                    if targetPlayer then
+                        sprayOnPlayer(targetPlayer)
+                    else
+                        print("Player not found.")
                     end
                 end
-            else
-                local xnmcvzxkcjaaaplayertarget = findPlayerByName(infinityGauntlet)
-                if xnmcvzxkcjaaaplayertarget then
-                    xnmcvzxkcjaaaplayerfunc(xnmcvzxkcjaaaplayertarget)
-                else
-                    print("Player not found.")
-                end
+                task.wait(15)
             end
-            task.wait(15)
+        else
+            sprayLoop = false
         end
     end
-    if Value == false then
-        xnmcvzxkcjaaaplayerloop = false
+})
+
+tab:textbox({
+    Name = "Custom Penis ID",
+    Callback = function(Value)
+        sprayId = tonumber(Value) or sprayId
     end
-end,})
+})
