f72cb4ded66c997f6ff09b75569aba2d45770dab
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-27 21:22:39 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,31 +1,32 @@
-function qweqqwwplayerfunc(qweqqwwplayertarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 8, qweqqwwplayertarget.Character.Head, qweqqwwplayertarget.Character.Head.CFrame * CFrame.new(0, 1.5, 0))
+function bnmbnbbplayerfunc(bnmbnbbplayertarget)
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 10000, bnmbnbbplayertarget.Character.LeftLowerArm, bnmbnbbplayertarget.Character.LeftLowerArm.CFrame * CFrame.new(0, 0, -1))
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 10000, bnmbnbbplayertarget.Character.RightLowerArm, bnmbnbbplayertarget.Character.RightLowerArm.CFrame * CFrame.new(0, 0, -1))
 end
 
-local qweqqwwplayerloop = false
+local bnmbnbbplayerloop = false
 
 tab:toggle({
-    Name = "Shake Camera",
+    Name = "Slick Back",
 		StartingState = false,
 		Description = "Spraypaint Toy Required",
 		Callback = function(Value)
    if Value == true then
-        qweqqwwplayerloop = true
-        while qweqqwwplayerloop do
+        bnmbnbbplayerloop = true
+        while bnmbnbbplayerloop do
             EquipSpray()
             task.wait(0.4)
             if fetargetname == "All" then
                 for _, v in pairs(game.Players:GetPlayers()) do
                     if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local qweqqwwplayertarget = v
-                        qweqqwwplayerfunc(qweqqwwplayertarget)
+                        local bnmbnbbplayertarget = v
+                        bnmbnbbplayerfunc(bnmbnbbplayertarget)
                         task.wait()
                     end
                 end
             else
-                local qweqqwwplayertarget = findPlayerByName(fetargetname)
-                if qweqqwwplayertarget then
-                    qweqqwwplayerfunc(qweqqwwplayertarget)
+                local bnmbnbbplayertarget = findPlayerByName(fetargetname)
+                if bnmbnbbplayertarget then
+                    bnmbnbbplayerfunc(bnmbnbbplayertarget)
                 else
                     print("Player not found.")
                 end
@@ -34,6 +35,6 @@ tab:toggle({
         end
     end
     if Value == false then
-        qweqqwwplayerloop = false
+        bnmbnbbplayerloop = false
     end
 end,})
