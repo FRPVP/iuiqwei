8137bab5f3a3a7aa41385b5747cdf8f21210549e
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-28 18:09:07 -0400

    Update library.lua

--- library.lua
+++ library.lua
@@ -697,7 +697,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "http://www.roblox.com/asset/?id=17646580928",
+		Image = "http://www.roblox.com/asset/?id=17651005728",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
