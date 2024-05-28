0e390bf7c936dc5852a97cf9ea5d7b01c4599496
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-27 20:02:51 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,39 +1,39 @@
-function andlxcaosplayerfunc(andlxcaosplayertarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Right, 2048, andlxcaosplayertarget.Character.HumanoidRootPart, andlxcaosplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 250, 0))
+function qweqqwwplayerfunc(qweqqwwplayertarget)
+    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Right, 10, qweqqwwplayertarget.Character.HumanoidRootPart, qweqqwwplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(99999, 99999, 99999))
 end
 
-local andlxcaosplayerloop = false
+local qweqqwwplayerloop = false
 
 tab:toggle({
-    Name = "Lag",
+    Name = "Yeet",
 		StartingState = false,
 		Description = "Spraypaint Toy Required",
 		Callback = function(Value)
    if Value == true then
-        andlxcaosplayerloop = true
-        while andlxcaosplayerloop do
+        qweqqwwplayerloop = true
+        while qweqqwwplayerloop do
             EquipSpray()
             task.wait(0.4)
             if fetargetname == "All" then
                 for _, v in pairs(game.Players:GetPlayers()) do
                     if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local andlxcaosplayertarget = v
-                        andlxcaosplayerfunc(andlxcaosplayertarget)
+                        local qweqqwwplayertarget = v
+                        qweqqwwplayerfunc(qweqqwwplayertarget)
                         task.wait()
                     end
                 end
             else
-                local andlxcaosplayertarget = findPlayerByName(fetargetname)
-                if andlxcaosplayertarget then
-                    andlxcaosplayerfunc(andlxcaosplayertarget)
+                local qweqqwwplayertarget = findPlayerByName(fetargetname)
+                if qweqqwwplayertarget then
+                    qweqqwwplayerfunc(qweqqwwplayertarget)
                 else
                     print("Player not found.")
                 end
             end
-            task.wait(0)
+            task.wait(15)
         end
     end
     if Value == false then
-        andlxcaosplayerloop = false
+        qweqqwwplayerloop = false
     end
 end,})
