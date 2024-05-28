3609c25844603ba49b099d35766f7b5baf505e8c
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-28 15:15:34 -0400

    Update library.lua

--- library.lua
+++ library.lua
@@ -893,7 +893,7 @@ function Library:create(options)
 	settingsTab:keybind{
 		Name = "Toggle Key",
 		Description = "Key to show/hide the UI.",
-		Keybind = Enum.KeyCode.LeftControl,
+		Keybind = Enum.KeyCode.LeftAlt,
 		Callback = function()
 			self.Toggled = not self.Toggled
 			Library:show(self.Toggled)
