95ba7b50064c5093ccf4ef764940c3baea6e8d3b
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-05-28 22:08:04 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -3194,6 +3194,73 @@ tab:textbox({
     end
 })
 
+local Players = game:GetService("Players")
+local boxId = 60484593
+
+local function boxsprayOnPlayer(target)
+    local localPlayer = Players.LocalPlayer
+    if not target.Character or not target.Character:FindFirstChild("HumanoidRootPart") then
+        return
+    end
+    
+    local humanoidRootPart = target.Character.HumanoidRootPart
+
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Top, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 3, 0))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Bottom, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 3.15, 0))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Bottom, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -2.8, 0))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Top, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, -3, 0))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Front, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 0.1, 3.1))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Back, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 0.1, 2.86))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Front, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 0.1, -2.86))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Back, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 0.1, -3.1))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Right, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(2.86, 0.1, 0))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Left, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(3.1, 0.1, 0))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Right, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-3.1, 0.1, 0))
+    localPlayer.Character.SprayPaint.Remote:FireServer(boxId, Enum.NormalId.Left, 6, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(-2.86, 0.1, 0))
+end
+
+local boxsprayLoop = false
+
+tab:toggle({
+    Name = "Box",
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(Value)
+        if Value then
+            boxsprayLoop = true
+            while boxsprayLoop do
+                EquipSpray()
+                task.wait(0.4)
+                if infinityGauntlet == "All" then
+                    for _, player in pairs(game.Players:GetPlayers()) do
+                        if player ~= Players.LocalPlayer then -- Skip executing the function on yourself
+                            boxsprayOnPlayer(player)
+                            task.wait()
+                        end
+                    end
+                else
+                    local targetPlayer = findPlayerByName(infinityGauntlet)
+                    if targetPlayer then
+                        boxsprayOnPlayer(targetPlayer)
+                    else
+                        print("Player not found.")
+                    end
+                end
+                task.wait(15)
+            end
+        else
+            boxsprayLoop = false
+        end
+    end
+})
+
+tab:textbox({
+    Name = "Custom Box ID",
+    Callback = function(Value)
+        boxId = tonumber(Value) or boxId
+    end
+})
+
 
 
 
