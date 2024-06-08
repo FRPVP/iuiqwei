6bbab032127e60954b7fd5eba53a610d9a8e470a
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 16:25:01 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,27 +1,27 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-local jadoiwanplayerloop = false
+local andlxcaosplayerloop = false
 
-function jadoiwanplayerfunc(jadoiwanplayertarget)
-  LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, jadoiwanplayertarget.Character.LeftHand, jadoiwanplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
+function andlxcaosplayerfunc(andlxcaosplayertarget)
+  LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Right, 2048, andlxcaosplayertarget.Character.HumanoidRootPart, andlxcaosplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 250, 0))
 end
 
 local function startLoop()
-    while jadoiwanplayerloop do
+    while andlxcaosplayerloop do
         EquipSpray()
         task.wait(0.4)
         if fetargetname == "All" then
             for _, v in pairs(Players:GetPlayers()) do
                 if v ~= LocalPlayer then -- Skip executing the function on yourself
-                    local jadoiwanplayertarget = v
-                    jadoiwanplayerfunc(jadoiwanplayertarget)
+                    local andlxcaosplayertarget = v
+                    andlxcaosplayerfunc(andlxcaosplayertarget)
                     task.wait()
                 end
             end
         else
-            local jadoiwanplayertarget = findPlayerByName(fetargetname)
-            if jadoiwanplayertarget then
-                jadoiwanplayerfunc(jadoiwanplayertarget)
+            local andlxcaosplayertarget = findPlayerByName(fetargetname)
+            if andlxcaosplayertarget then
+                andlxcaosplayerfunc(andlxcaosplayertarget)
             else
                 print("Player not found.")
             end
@@ -31,7 +31,7 @@ local function startLoop()
 end
 
 local function onCharacterAdded(character)
-    if jadoiwanplayerloop then
+    if andlxcaosplayerloop then
         task.spawn(startLoop)
     end
 end
@@ -39,12 +39,12 @@ end
 LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
-    Name = "Freeze",
+    Name = "Lag",
     StartingState = false,
     Description = "Spraypaint Toy Required",
-    Callback = function(jadoiwanplayer)
-        jadoiwanplayerloop = jadoiwanplayer
-        if jadoiwanplayer then
+    Callback = function(andlxcaosplayer)
+        andlxcaosplayerloop = andlxcaosplayer
+        if andlxcaosplayer then
             task.spawn(startLoop)
         end
     end,
