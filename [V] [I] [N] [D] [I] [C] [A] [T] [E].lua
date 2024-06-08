6349a5f47601d185870fd1a8fded2ae7611608b0
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 16:01:56 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -1921,45 +1921,57 @@ tab:button({
     end,
 })
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local resetplayerloop = false
+
 function resetplayerfunc(resetplayertarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, resetplayertarget.Character.Head, resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, resetplayertarget.Character.Head, resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
 end
 
-local resetplayerloop = false
+local function startLoop()
+    while resetplayerloop do
+        EquipSpray()
+        task.wait(0.4)
+        if fetargetname == "All" then
+            for _, v in pairs(Players:GetPlayers()) do
+                if v ~= LocalPlayer then -- Skip executing the function on yourself
+                    local resetplayertarget = v
+                    resetplayerfunc(resetplayertarget)
+                    task.wait()
+                end
+            end
+        else
+            local resetplayertarget = findPlayerByName(fetargetname)
+            if resetplayertarget then
+                resetplayerfunc(resetplayertarget)
+            else
+                print("Player not found.")
+            end
+        end
+        task.wait(0)
+    end
+end
+
+local function onCharacterAdded(character)
+    if resetplayerloop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
     Name = "Loop Reset",
-		StartingState = false,
-		Description = "Spraypaint Toy Required",
-		Callback = function(resetplayer)
-    if resetplayer == true then
-        resetplayerloop = true
-        while resetplayerloop do
-            EquipSpray()
-            task.wait(0.4)
-            if fetargetname == "All" then
-                for _, v in pairs(game.Players:GetPlayers()) do
-                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local resetplayertarget = v
-                        resetplayerfunc(resetplayertarget)
-                        task.wait()
-                    end
-                end
-            else
-                local resetplayertarget = findPlayerByName(fetargetname)
-                if resetplayertarget then
-                    resetplayerfunc(resetplayertarget)
-                else
-                    print("Player not found.")
-                end
-            end
-            task.wait(0)
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(resetplayer)
+        resetplayerloop = resetplayer
+        if resetplayer then
+            task.spawn(startLoop)
         end
-    end
-    if resetplayer == false then
-        resetplayerloop = false
-    end
-end,})
+    end,
+})
 
 function collideplayerfunc(collideplayertarget)
     game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, collideplayertarget.Character.HumanoidRootPart, collideplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
