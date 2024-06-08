971d0661d249d627be6146c036659c57b756a4a2
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 16:25:40 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -2247,45 +2247,57 @@ tab:toggle({
     end,
 })
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local andlxcaosplayerloop = false
+
 function andlxcaosplayerfunc(andlxcaosplayertarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Right, 2048, andlxcaosplayertarget.Character.HumanoidRootPart, andlxcaosplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 250, 0))
+  LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Right, 2048, andlxcaosplayertarget.Character.HumanoidRootPart, andlxcaosplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 250, 0))
 end
 
-local andlxcaosplayerloop = false
+local function startLoop()
+    while andlxcaosplayerloop do
+        EquipSpray()
+        task.wait(0.4)
+        if fetargetname == "All" then
+            for _, v in pairs(Players:GetPlayers()) do
+                if v ~= LocalPlayer then -- Skip executing the function on yourself
+                    local andlxcaosplayertarget = v
+                    andlxcaosplayerfunc(andlxcaosplayertarget)
+                    task.wait()
+                end
+            end
+        else
+            local andlxcaosplayertarget = findPlayerByName(fetargetname)
+            if andlxcaosplayertarget then
+                andlxcaosplayerfunc(andlxcaosplayertarget)
+            else
+                print("Player not found.")
+            end
+        end
+        task.wait(0)
+    end
+end
+
+local function onCharacterAdded(character)
+    if andlxcaosplayerloop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
     Name = "Lag",
-		StartingState = false,
-		Description = "Spraypaint Toy Required",
-		Callback = function(Value)
-   if Value == true then
-        andlxcaosplayerloop = true
-        while andlxcaosplayerloop do
-            EquipSpray()
-            task.wait(0.4)
-            if fetargetname == "All" then
-                for _, v in pairs(game.Players:GetPlayers()) do
-                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local andlxcaosplayertarget = v
-                        andlxcaosplayerfunc(andlxcaosplayertarget)
-                        task.wait()
-                    end
-                end
-            else
-                local andlxcaosplayertarget = findPlayerByName(fetargetname)
-                if andlxcaosplayertarget then
-                    andlxcaosplayerfunc(andlxcaosplayertarget)
-                else
-                    print("Player not found.")
-                end
-            end
-            task.wait(0)
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(andlxcaosplayer)
+        andlxcaosplayerloop = andlxcaosplayer
+        if andlxcaosplayer then
+            task.spawn(startLoop)
         end
-    end
-    if Value == false then
-        andlxcaosplayerloop = false
-    end
-end,})
+    end,
+})
 
 function iopiipiopolllplayerfunc(iopiipiopolllplayertarget)
     game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 2500, iopiipiopolllplayertarget.Character.RightHand, iopiipiopolllplayertarget.Character.RightHand.CFrame * CFrame.new(0, -50, 0))
