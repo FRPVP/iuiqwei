5fbb11db3c9ea065b1bc3436ef0e8663f9b2db26
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 16:09:36 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,37 +1,37 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-local resetplayerloop = false
+local collideplayerloop = false
 
-function resetplayerfunc(resetplayertarget)
-    LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, resetplayertarget.Character.Head, resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+function collideplayerfunc(collideplayertarget)
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, collideplayertarget.Character.HumanoidRootPart, collideplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
 end
 
 local function startLoop()
-    while resetplayerloop do
+    while collideplayerloop do
         EquipSpray()
         task.wait(0.4)
         if fetargetname == "All" then
             for _, v in pairs(Players:GetPlayers()) do
                 if v ~= LocalPlayer then -- Skip executing the function on yourself
-                    local resetplayertarget = v
-                    resetplayerfunc(resetplayertarget)
+                    local collideplayertarget = v
+                    collideplayerfunc(collideplayertarget)
                     task.wait()
                 end
             end
         else
-            local resetplayertarget = findPlayerByName(fetargetname)
-            if resetplayertarget then
-                resetplayerfunc(resetplayertarget)
+            local collideplayertarget = findPlayerByName(fetargetname)
+            if collideplayertarget then
+                collideplayerfunc(collideplayertarget)
             else
                 print("Player not found.")
             end
         end
-        task.wait(0)
+        task.wait(15)
     end
 end
 
 local function onCharacterAdded(character)
-    if resetplayerloop then
+    if collideplayerloop then
         task.spawn(startLoop)
     end
 end
@@ -39,12 +39,12 @@ end
 LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
-    Name = "Loop Reset",
+    Name = "Remove Collisions",
     StartingState = false,
     Description = "Spraypaint Toy Required",
-    Callback = function(resetplayer)
-        resetplayerloop = resetplayer
-        if resetplayer then
+    Callback = function(collideplayer)
+        collideplayerloop = collideplayer
+        if collideplayer then
             task.spawn(startLoop)
         end
     end,
