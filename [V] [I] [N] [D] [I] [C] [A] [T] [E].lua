59cdc194652d8d8ae25cc9a852c4b8a0460c7bb9
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 18:38:37 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -2602,45 +2602,56 @@ tab:toggle({
     end,
 })
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local djiwbdjksbbxkzplayerloop = false
+
 function djiwbdjksbbxkzplayerfunc(djiwbdjksbbxkzplayertarget)
-game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(14976845153, Enum.NormalId.Front, 1.5, (djiwbdjksbbxkzplayertarget.Character.HumanoidRootPart), djiwbdjksbbxkzplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,-1.15,-2.90))
+LocalPlayer.Character.SprayPaint.Remote:FireServer(14976845153, Enum.NormalId.Front, 1.5, (djiwbdjksbbxkzplayertarget.Character.HumanoidRootPart), djiwbdjksbbxkzplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0,-1.15,-2.90))
 end
 
-local djiwbdjksbbxkzplayerloop = false
+local function startLoop()
+    while djiwbdjksbbxkzplayerloop do
+        EquipSpray()
+        task.wait(0.4)
+        if fetargetname == "All" then
+            for _, v in pairs(Players:GetPlayers()) do
+                    local djiwbdjksbbxkzplayertarget = v
+                    djiwbdjksbbxkzplayerfunc(djiwbdjksbbxkzplayertarget)
+                    task.wait()
+                end
+            end
+        else
+            local djiwbdjksbbxkzplayertarget = findPlayerByName(infinityGauntlet)
+            if djiwbdjksbbxkzplayertarget then
+                djiwbdjksbbxkzplayerfunc(djiwbdjksbbxkzplayertarget)
+            else
+                print("Player not found.")
+            end
+        end
+        task.wait(15)
+    end
+end
+
+local function onCharacterAdded(character)
+    if djiwbdjksbbxkzplayerloop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
     Name = "Cum On Penis",
-		StartingState = false,
-		Description = "Spraypaint Toy Required",
-		Callback = function(Value)
-   if Value == true then
-        djiwbdjksbbxkzplayerloop = true
-        while djiwbdjksbbxkzplayerloop do
-            EquipSpray()
-            task.wait(0.4)
-            if infinityGauntlet == "All" then
-                for _, v in pairs(game.Players:GetPlayers()) do
-                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local djiwbdjksbbxkzplayertarget = v
-                        djiwbdjksbbxkzplayerfunc(djiwbdjksbbxkzplayertarget)
-                        task.wait()
-                    end
-                end
-            else
-                local djiwbdjksbbxkzplayertarget = findPlayerByName(infinityGauntlet)
-                if djiwbdjksbbxkzplayertarget then
-                    djiwbdjksbbxkzplayerfunc(djiwbdjksbbxkzplayertarget)
-                else
-                    print("Player not found.")
-                end
-            end
-            task.wait(15)
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(djiwbdjksbbxkzplayer)
+        djiwbdjksbbxkzplayerloop = djiwbdjksbbxkzplayer
+        if djiwbdjksbbxkzplayer then
+            task.spawn(startLoop)
         end
-    end
-    if Value == false then
-        djiwbdjksbbxkzplayerloop = false
-    end
-end,})
+    end,
+})
 
 local Players = game:GetService("Players")
 local sprayId = 60484593
