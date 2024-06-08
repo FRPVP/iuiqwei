a132e5b555ea5b66c92e9becc63baed498c66c30
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 16:33:27 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -2404,46 +2404,58 @@ tab:toggle({
     end,
 })
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local bnmbnbbplayerloop = false
+
 function bnmbnbbplayerfunc(bnmbnbbplayertarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 10000, bnmbnbbplayertarget.Character.LeftLowerArm, bnmbnbbplayertarget.Character.LeftLowerArm.CFrame * CFrame.new(0, 0, -1))
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 10000, bnmbnbbplayertarget.Character.RightLowerArm, bnmbnbbplayertarget.Character.RightLowerArm.CFrame * CFrame.new(0, 0, -1))
+  LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 10000, bnmbnbbplayertarget.Character.LeftLowerArm, bnmbnbbplayertarget.Character.LeftLowerArm.CFrame * CFrame.new(0, 0, -1))
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 10000, bnmbnbbplayertarget.Character.RightLowerArm, bnmbnbbplayertarget.Character.RightLowerArm.CFrame * CFrame.new(0, 0, -1))
 end
 
-local bnmbnbbplayerloop = false
+local function startLoop()
+    while bnmbnbbplayerloop do
+        EquipSpray()
+        task.wait(0.4)
+        if fetargetname == "All" then
+            for _, v in pairs(Players:GetPlayers()) do
+                if v ~= LocalPlayer then -- Skip executing the function on yourself
+                    local bnmbnbbplayertarget = v
+                    bnmbnbbplayerfunc(bnmbnbbplayertarget)
+                    task.wait()
+                end
+            end
+        else
+            local bnmbnbbplayertarget = findPlayerByName(fetargetname)
+            if bnmbnbbplayertarget then
+                bnmbnbbplayerfunc(bnmbnbbplayertarget)
+            else
+                print("Player not found.")
+            end
+        end
+        task.wait(15)
+    end
+end
+
+local function onCharacterAdded(character)
+    if bnmbnbbplayerloop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
     Name = "Slick Movement",
-		StartingState = false,
-		Description = "Spraypaint Toy Required",
-		Callback = function(Value)
-   if Value == true then
-        bnmbnbbplayerloop = true
-        while bnmbnbbplayerloop do
-            EquipSpray()
-            task.wait(0.4)
-            if fetargetname == "All" then
-                for _, v in pairs(game.Players:GetPlayers()) do
-                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local bnmbnbbplayertarget = v
-                        bnmbnbbplayerfunc(bnmbnbbplayertarget)
-                        task.wait()
-                    end
-                end
-            else
-                local bnmbnbbplayertarget = findPlayerByName(fetargetname)
-                if bnmbnbbplayertarget then
-                    bnmbnbbplayerfunc(bnmbnbbplayertarget)
-                else
-                    print("Player not found.")
-                end
-            end
-            task.wait(15)
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(bnmbnbbplayer)
+        bnmbnbbplayerloop = bnmbnbbplayer
+        if bnmbnbbplayer then
+            task.spawn(startLoop)
         end
-    end
-    if Value == false then
-        bnmbnbbplayerloop = false
-    end
-end,})
+    end,
+})
 
 
 
