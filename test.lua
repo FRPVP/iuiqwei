33c3cd85270d4c220f982c154842c86ffc0b6ae0
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 16:27:15 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,37 +1,38 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-local andlxcaosplayerloop = false
+local iopiipiopolllplayerloop = false
 
-function andlxcaosplayerfunc(andlxcaosplayertarget)
-  LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Right, 2048, andlxcaosplayertarget.Character.HumanoidRootPart, andlxcaosplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 250, 0))
+function iopiipiopolllplayerfunc(iopiipiopolllplayertarget)
+  LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 2500, iopiipiopolllplayertarget.Character.RightHand, iopiipiopolllplayertarget.Character.RightHand.CFrame * CFrame.new(0, -50, 0))
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Front, 2500, iopiipiopolllplayertarget.Character.RightHand, iopiipiopolllplayertarget.Character.RightHand.CFrame * CFrame.new(0, 50, 0))
 end
 
 local function startLoop()
-    while andlxcaosplayerloop do
+    while iopiipiopolllplayerloop do
         EquipSpray()
         task.wait(0.4)
         if fetargetname == "All" then
             for _, v in pairs(Players:GetPlayers()) do
                 if v ~= LocalPlayer then -- Skip executing the function on yourself
-                    local andlxcaosplayertarget = v
-                    andlxcaosplayerfunc(andlxcaosplayertarget)
+                    local iopiipiopolllplayertarget = v
+                    iopiipiopolllplayerfunc(iopiipiopolllplayertarget)
                     task.wait()
                 end
             end
         else
-            local andlxcaosplayertarget = findPlayerByName(fetargetname)
-            if andlxcaosplayertarget then
-                andlxcaosplayerfunc(andlxcaosplayertarget)
+            local iopiipiopolllplayertarget = findPlayerByName(fetargetname)
+            if iopiipiopolllplayertarget then
+                iopiipiopolllplayerfunc(iopiipiopolllplayertarget)
             else
                 print("Player not found.")
             end
         end
-        task.wait(0)
+        task.wait(15)
     end
 end
 
 local function onCharacterAdded(character)
-    if andlxcaosplayerloop then
+    if iopiipiopolllplayerloop then
         task.spawn(startLoop)
     end
 end
@@ -39,12 +40,12 @@ end
 LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
-    Name = "Lag",
+    Name = "Launch Up",
     StartingState = false,
     Description = "Spraypaint Toy Required",
-    Callback = function(andlxcaosplayer)
-        andlxcaosplayerloop = andlxcaosplayer
-        if andlxcaosplayer then
+    Callback = function(iopiipiopolllplayer)
+        iopiipiopolllplayerloop = iopiipiopolllplayer
+        if iopiipiopolllplayer then
             task.spawn(startLoop)
         end
     end,
