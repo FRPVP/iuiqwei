47294417f2cc3bc2404c2fad56db83413300fb9f
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-08 21:14:48 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -1979,45 +1979,55 @@ tab:toggle({
     end,
 })
 
+local collideplayerloop = false
+
 function collideplayerfunc(collideplayertarget)
-    o.LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, collideplayertarget.Character.HumanoidRootPart, collideplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
+   LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, collideplayertarget.Character.HumanoidRootPart, collideplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
 end
 
-local collideplayerloop = false
+local function startLoop()
+    while collideplayerloop do
+        EquipSpray()
+        task.wait(0.4)
+        if fetargetname == "All" then
+            for _, v in pairs(Players:GetPlayers()) do
+                if v ~= LocalPlayer then -- Skip executing the function on yourself
+                    local collideplayertarget = v
+                    collideplayerfunc(collideplayertarget)
+                    task.wait()
+                end
+            end
+        else
+            local collideplayertarget = findPlayerByName(fetargetname)
+            if collideplayertarget then
+                collideplayerfunc(collideplayertarget)
+            else
+                print("Player not found.")
+            end
+        end
+        task.wait(0)
+    end
+end
+
+local function onCharacterAdded(character)
+    if collideplayerloop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
     Name = "Remove Collisions",
-		StartingState = false,
-		Description = "Spraypaint Toy Required",
-		Callback = function(Value)
-   if Value == true then
-        collideplayerloop = true
-        while collideplayerloop do
-            EquipSpray()
-            task.wait(0.4)
-            if fetargetname == "All" then
-                for _, v in pairs(game.Players:GetPlayers()) do
-                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local collideplayertarget = v
-                        collideplayerfunc(collideplayertarget)
-                        task.wait()
-                    end
-                end
-            else
-                local collideplayertarget = findPlayerByName(fetargetname)
-                if collideplayertarget then
-                    collideplayerfunc(collideplayertarget)
-                else
-                    print("Player not found.")
-                end
-            end
-            task.wait(15)
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(collideplayer)
+        collideplayerloop = collideplayer
+        if collideplayer then
+            task.spawn(startLoop)
         end
-    end
-    if Value == false then
-        collideplayerloop = false
-    end
-end,})
+    end,
+})
 
 local poolplayerloop = false
 
