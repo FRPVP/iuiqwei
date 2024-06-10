3d569ca15c785805ef55af41283b36f2f0626105
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-10 18:21:55 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -1737,46 +1737,56 @@ tab:toggle({
     end,
 })
 
+local fesliploop = false
+
 function feslipfunc(fesliptarget)
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, fesliptarget.Character.LeftUpperLeg, fesliptarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, fesliptarget.Character.LeftUpperLeg, fesliptarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
+LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, fesliptarget.Character.LeftUpperLeg, fesliptarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, fesliptarget.Character.LeftUpperLeg, fesliptarget.Character.LeftUpperLeg.CFrame * CFrame.new(0, -2, 0))
 end
 
-local fesliploop = false
+local function startLoop()
+    while fesliploop do
+        EquipSpray()
+        task.wait(0.4)
+        if fetargetname == "All" then
+            for _, v in pairs(Players:GetPlayers()) do
+                if v ~= LocalPlayer then -- Skip executing the function on yourself
+                    local fesliptarget = v
+                    feslipfunc(fesliptarget)
+                    task.wait()
+                end
+            end
+        else
+            local fesliptarget = findPlayerByName(fetargetname)
+            if fesliptarget then
+                feslipfunc(fesliptarget)
+            else
+                print("Player not found.")
+            end
+        end
+        task.wait(15)
+    end
+end
+
+local function onCharacterAdded(character)
+    if fesliploop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
     Name = "Slip Up",
-		StartingState = false,
-		Description = "Spraypaint Toy Required",
-		Callback = function(Value)
-   if Value == true then
-        fesliploop = true
-        while fesliploop do
-            EquipSpray()
-            task.wait(0.4)
-            if fetargetname == "All" then
-                for _, v in pairs(game.Players:GetPlayers()) do
-                    if v ~= game.Players.LocalPlayer then -- Skip executing the function on yourself
-                        local fesliptarget = v
-                        feslipfunc(fesliptarget)
-                        task.wait()
-                    end
-                end
-            else
-                local fesliptarget = findPlayerByName(fetargetname)
-                if fesliptarget then
-                    feslipfunc(fesliptarget)
-                else
-                    print("Player not found.")
-                end
-            end
-            task.wait(15)
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(feslip)
+        fesliploop = feslip
+        if feslip then
+            task.spawn(startLoop)
         end
-    end
-    if Value == false then
-        fesliploop = false
-    end
-end,})
+    end,
+})
 
 function opiwhdaplayerfunc(opiwhdaplayertarget)
     game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Back, 6.331, opiwhdaplayertarget.Character.HumanoidRootPart, opiwhdaplayertarget.Character.HumanoidRootPart.CFrame * CFrame.new(0, 99999, 0))
