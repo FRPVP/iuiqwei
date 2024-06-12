8a848dcfb5564b884385b9249ad025124624edb2
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-12 17:00:58 -0400

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -8,7 +8,9 @@ function heatplayerfunc(heatplayertarget)
     elseif game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("SprayPaint") then
         game:GetService("Players").LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
          game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
-        
+        game.Players.LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
+    elseif game:GetService("Players").LocalPlayer.Character:FindFirstChild("SprayPaint") then
+         game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
 end
 
 tab:button({
@@ -16,16 +18,12 @@ tab:button({
     Description = "Spraypaint Toy Required",
     Callback = function()
         if fetargetname == "All" then
-        EquipSpray() -- Equip the spray first
-        -- Iterate over all players and execute heatplayerfunc on each player except yourself
         for _, player in pairs(game:GetService("Players"):GetPlayers()) do
             if player ~= game.Players.LocalPlayer then
                 heatplayerfunc(player)
             end
         end
     elseif fetargetname ~= "" then
-        EquipSpray() -- Equip the spray first
-        -- Find the player with the selected name
         local heatplayertarget = findPlayerByName(fetargetname)
         if heatplayertarget then
             heatplayerfunc(heatplayertarget) -- Execute heatplayerfunc on the player
