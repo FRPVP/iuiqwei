9b549f5f0c09408fb3f4393fff46801540affa09
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-13 10:21:42 -0400

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -1,37 +1,60 @@
-function heatplayerfunc(heatplayertarget)
-    if game:GetService("Players").LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
-        game:GetService("ReplicatedStorage").Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
-        game:GetService("ReplicatedStorage").Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
-        game:GetService("Players").LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
-    game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
-        game.Players.LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
-    elseif game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("SprayPaint") then
-        game:GetService("Players").LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
-         game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
-        game.Players.LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
-    elseif game:GetService("Players").LocalPlayer.Character:FindFirstChild("SprayPaint") then
-         game:GetService("Players").LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+local resetplayerloop = false
+
+function resetplayerfunc(resetplayertarget)
+	    if LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
+        Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
+        Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
+        LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, resetplayertarget.Character.Head, resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+		LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
+    elseif LocalPlayer.Backpack:FindFirstChild("SprayPaint") then
+        LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
+		LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, resetplayertarget.Character.Head, resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+		LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
+    elseif LocalPlayer.Character:FindFirstChild("SprayPaint") then
+		LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, resetplayertarget.Character.Head, resetplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+end
 end
 
-tab:button({
-    Name = "Reset",
-    Description = "Spraypaint Toy Required",
-    Callback = function()
+local function startLoop()
+    while resetplayerloop do
+        task.wait(0.4)
         if fetargetname == "All" then
-        for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-            if player ~= game.Players.LocalPlayer then
-                heatplayerfunc(player)
+            for _, v in pairs(Players:GetPlayers()) do
+                if v ~= LocalPlayer then -- Skip executing the function on yourself
+                    local resetplayertarget = v
+                    resetplayerfunc(resetplayertarget)
+                    task.wait()
+                end
+            end
+        else
+            local resetplayertarget = findPlayerByName(fetargetname)
+            if resetplayertarget then
+                resetplayerfunc(resetplayertarget)
+            else
+                print("Player not found.")
             end
         end
-    elseif fetargetname ~= "" then
-        local heatplayertarget = findPlayerByName(fetargetname)
-        if heatplayertarget then
-            heatplayerfunc(heatplayertarget) -- Execute heatplayerfunc on the player
-        else
-            print("Player not found.")
-        end
-    else
-        print("Please select a name from the dropdown.")
+        task.wait(0)
     end
+end
+
+local function onCharacterAdded(character)
+    if resetplayerloop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
+
+tab:toggle({
+    Name = "Loop Reset",
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(resetplayer)
+        resetplayerloop = resetplayer
+        if resetplayer then
+            task.spawn(startLoop)
+        end
     end,
 })
