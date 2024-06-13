023872c570264a8a99a0121701bcca9535212c49
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-13 11:51:17 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -2564,10 +2564,10 @@ tab:textbox({
     end
 })
 
-local Players = game:GetService("Players")
 local tagId = 60484593
+local tagsprayLoop = false
 
-local function tagsprayOnPlayer(target)
+local function sprayOnPlayer(target)
     local localPlayer = Players.LocalPlayer
     if not target.Character or not target.Character:FindFirstChild("HumanoidRootPart") then
         return
@@ -2575,42 +2575,62 @@ local function tagsprayOnPlayer(target)
     
     local humanoidRootPart = target.Character.HumanoidRootPart
 
+	if LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
+        Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
+        Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
+        LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
     localPlayer.Character.SprayPaint.Remote:FireServer(tagId, Enum.NormalId.Front, 5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 5, 0))
+LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
+    elseif LocalPlayer.Backpack:FindFirstChild("SprayPaint") then
+        LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
+    localPlayer.Character.SprayPaint.Remote:FireServer(tagId, Enum.NormalId.Front, 5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 5, 0))
+LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
+    elseif LocalPlayer.Character:FindFirstChild("SprayPaint") then
+    localPlayer.Character.SprayPaint.Remote:FireServer(tagId, Enum.NormalId.Front, 5, humanoidRootPart, humanoidRootPart.CFrame * CFrame.new(0, 5, 0))
+	
+end
 end
 
-local tagsprayLoop = false
+local function starttagsprayLoop()
+    while tagsprayLoop do
+        task.wait(0.4)
+        if infinityGauntlet == "All" then
+            for _, player in pairs(Players:GetPlayers()) do
+                if player ~= Players.LocalPlayer then -- Skip executing the function on yourself
+                    sprayOnPlayer(player)
+                    task.wait()
+                end
+            end
+        else
+            local targetPlayer = findPlayerByName(infinityGauntlet)
+            if targetPlayer then
+                sprayOnPlayer(targetPlayer)
+            else
+                print("Player not found.")
+            end
+        end
+        task.wait(15)
+    end
+end
+
+local function onCharacterAdded(character)
+    if tagsprayLoop then
+        task.spawn(starttagsprayLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 tab:toggle({
     Name = "Tag",
     StartingState = false,
     Description = "Spraypaint Toy Required",
     Callback = function(Value)
+        tagsprayLoop = Value
         if Value then
-            tagsprayLoop = true
-            while tagsprayLoop do
-                EquipSpray()
-                task.wait(0.4)
-                if infinityGauntlet == "All" then
-                    for _, player in pairs(game.Players:GetPlayers()) do
-                        if player ~= Players.LocalPlayer then -- Skip executing the function on yourself
-                            tagsprayOnPlayer(player)
-                            task.wait()
-                        end
-                    end
-                else
-                    local targetPlayer = findPlayerByName(infinityGauntlet)
-                    if targetPlayer then
-                        tagsprayOnPlayer(targetPlayer)
-                    else
-                        print("Player not found.")
-                    end
-                end
-                task.wait(15)
-            end
-        else
-            tagsprayLoop = false
+            task.spawn(starttagsprayLoop)
         end
-    end
+    end,
 })
 
 tab:textbox({
@@ -2620,8 +2640,6 @@ tab:textbox({
     end
 })
 
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local defaultSprayId = 12781220539  -- Default spray ID for the block
 
 local function giveTool()
@@ -2639,8 +2657,8 @@ local function giveTool()
             local sprayPaint
 
             if backpack.Toys:FindFirstChild("SprayPaint") then
-                ReplicatedStorage.Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
-                ReplicatedStorage.Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
+                Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
+                Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
                 sprayPaint = backpack:FindFirstChild("SprayPaint")
                 if sprayPaint then
                     sprayPaint.Parent = character
