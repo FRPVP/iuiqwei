79b2df35fb79c3ef2913f1ac9d02f794b9ddc3f3
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-15 17:17:32 -0400

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -1,33 +1,33 @@
-function heatplayerfunc(heatplayertarget)
+function zxnjckskooplayerfunc(zxnjckskooplayertarget)
     if LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
         Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
         Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
         LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
-    LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, zxnjckskooplayertarget.Character.LeftHand, zxnjckskooplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
         LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
     elseif LocalPlayer.Backpack:FindFirstChild("SprayPaint") then
         LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
-         LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+         LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, zxnjckskooplayertarget.Character.LeftHand, zxnjckskooplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
         LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
     elseif LocalPlayer.Character:FindFirstChild("SprayPaint") then
-         LocalPlayer.Character.SprayPaint.Remote:FireServer(80373024, Enum.NormalId.Back, 15, (heatplayertarget.Character.Head), heatplayertarget.Character.Head.CFrame * CFrame.new(0, math.huge, 0))
+         LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, zxnjckskooplayertarget.Character.LeftHand, zxnjckskooplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
 end
 end
 
 tab:button({
-    Name = "Reset",
+    Name = "Freeze",
     Description = "Spraypaint Toy Required",
     Callback = function()
         if fetargetname == "All" then
         for _, player in pairs(game:GetService("Players"):GetPlayers()) do
             if player ~= game.Players.LocalPlayer then
-                heatplayerfunc(player)
+                zxnjckskooplayerfunc(player)
             end
         end
     elseif fetargetname ~= "" then
-        local heatplayertarget = findPlayerByName(fetargetname)
-        if heatplayertarget then
-            heatplayerfunc(heatplayertarget) -- Execute heatplayerfunc on the player
+        local zxnjckskooplayertarget = findPlayerByName(fetargetname)
+        if zxnjckskooplayertarget then
+            zxnjckskooplayerfunc(zxnjckskooplayertarget) -- Execute zxnjckskooplayerfunc on the player
         else
             print("Player not found.")
         end
