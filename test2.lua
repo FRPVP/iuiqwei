52ba18a843725ea06b87236d026a03b25541d88f
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-06-16 10:12:58 -0400

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -1,38 +1,63 @@
-function zxnjckskooplayerfunc(zxnjckskooplayertarget)
-    if LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
+local ancoawpqjwejejjloop = false
+
+function ancoawpqjwejejjfunc(ancoawpqjwejejjtarget)
+	if LocalPlayer.Backpack.Toys:FindFirstChild("SprayPaint") then
         Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
         Remotes.Extras.ReplicateToy:InvokeServer("SprayPaint")
         LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
-LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, zxnjckskooplayertarget.Character.LeftHand, zxnjckskooplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
-        LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
+LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, ancoawpqjwejejjtarget.Character.RightHand, ancoawpqjwejejjtarget.Character.RightHand.CFrame * CFrame.new(0, -2, 0))
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, ancoawpqjwejejjtarget.Character.LeftHand, ancoawpqjwejejjtarget.Character.LeftHand.CFrame * CFrame.new(0, -2, 0))
+LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
     elseif LocalPlayer.Backpack:FindFirstChild("SprayPaint") then
         LocalPlayer.Backpack.SprayPaint.Parent = game.Players.LocalPlayer.Character
-         LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, zxnjckskooplayertarget.Character.LeftHand, zxnjckskooplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
-        LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
+	LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, ancoawpqjwejejjtarget.Character.RightHand, ancoawpqjwejejjtarget.Character.RightHand.CFrame * CFrame.new(0, -2, 0))
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, ancoawpqjwejejjtarget.Character.LeftHand, ancoawpqjwejejjtarget.Character.LeftHand.CFrame * CFrame.new(0, -2, 0))
+LocalPlayer.Character.SprayPaint.Parent = game:GetService("Players").LocalPlayer.Backpack
     elseif LocalPlayer.Character:FindFirstChild("SprayPaint") then
-         LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 2048, zxnjckskooplayertarget.Character.LeftHand, zxnjckskooplayertarget.Character.LeftHand.CFrame * CFrame.new(0, 0, 0))
+	LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, ancoawpqjwejejjtarget.Character.RightHand, ancoawpqjwejejjtarget.Character.RightHand.CFrame * CFrame.new(0, -2, 0))
+    LocalPlayer.Character.SprayPaint.Remote:FireServer(0, Enum.NormalId.Top, 6.331, ancoawpqjwejejjtarget.Character.LeftHand, ancoawpqjwejejjtarget.Character.LeftHand.CFrame * CFrame.new(0, -2, 0))
 end
 end
 
-tab:button({
-    Name = "Freeze",
-    Description = "Spraypaint Toy Required",
-    Callback = function()
+local function startLoop()
+    while ancoawpqjwejejjloop do
+        task.wait(0.4)
         if fetargetname == "All" then
-        for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-            if player ~= game.Players.LocalPlayer then
-                zxnjckskooplayerfunc(player)
+            for _, v in pairs(Players:GetPlayers()) do
+                if v ~= LocalPlayer then -- Skip executing the function on yourself
+                    local ancoawpqjwejejjtarget = v
+                    ancoawpqjwejejjfunc(ancoawpqjwejejjtarget)
+                    task.wait()
+                end
+            end
+        else
+            local ancoawpqjwejejjtarget = findPlayerByName(fetargetname)
+            if ancoawpqjwejejjtarget then
+                ancoawpqjwejejjfunc(ancoawpqjwejejjtarget)
+            else
+                print("Player not found.")
             end
         end
-    elseif fetargetname ~= "" then
-        local zxnjckskooplayertarget = findPlayerByName(fetargetname)
-        if zxnjckskooplayertarget then
-            zxnjckskooplayerfunc(zxnjckskooplayertarget) -- Execute zxnjckskooplayerfunc on the player
-        else
-            print("Player not found.")
-        end
-    else
-        print("Please select a name from the dropdown.")
+        task.wait(0)
     end
+end
+
+local function onCharacterAdded(character)
+    if ancoawpqjwejejjloop then
+        task.spawn(startLoop)
+    end
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
+
+tab:toggle({
+    Name = "test",
+    StartingState = false,
+    Description = "Spraypaint Toy Required",
+    Callback = function(ancoawpqjwejejj)
+        ancoawpqjwejejjloop = ancoawpqjwejejj
+        if ancoawpqjwejejj then
+            task.spawn(startLoop)
+        end
     end,
 })
