3d2ada35b2db8cd031241656ae390cb1646af26f
FRPVP <167589931+FRPVP@users.noreply.github.com>
2024-07-16 13:05:36 -0400

    Update [V] [I] [N] [D] [I] [C] [A] [T] [E].lua

--- [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
+++ [V] [I] [N] [D] [I] [C] [A] [T] [E].lua
@@ -1,1 +1,66 @@
-later
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FRPVP/iuiqwei/main/library.lua"))()
+
+local gui = Library:create{
+    Theme = Library.Themes.Vindicate
+}
+
+local tab = gui:tab{
+    Icon = "rbxassetid://18519217818",
+    Name = "Murder Mystery 2"
+}
+
+tab:button({
+    Name = "Activate Script",
+    Description = "",
+    Callback = function()
+        print("Button clicked")
+    end,
+})
+
+
+
+local tab = gui:tab{
+    Icon = "rbxassetid://18519224545",
+    Name = "Loomian Legacy"
+}
+
+tab:button({
+    Name = "Activate Script",
+    Description = "",
+    Callback = function()
+        print("Button clicked")
+    end,
+})
+
+
+
+
+local tab = gui:tab{
+    Icon = "rbxassetid://18519214713",
+    Name = "Lucky Block Battlegrounds"
+}
+
+tab:button({
+    Name = "Activate Script",
+    Description = "",
+    Callback = function()
+        print("Button clicked")
+    end,
+})
+
+
+
+local tab = gui:tab{
+    Icon = "rbxassetid://18519221386",
+    Name = "Flee The Facility"
+}
+
+
+
+tab:button({
+    Name = "Activate Script",
+    Description = "",
+    Callback = function()
+        print("Button clicked")
+    end,
+})
